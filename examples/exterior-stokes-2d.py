2503530f320e945a99b9a48b9ce4ef107bb8ecc8
Natalie Beams <beams2@illinois.edu>
2017-03-03 15:12:42 -0600

    flake8 fixes and add exterior stokes example

---
+++ examples/exterior-stokes-2d.py
@@ -0,0 +1,211 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.clmath  # noqa
+
+from meshmode.discretization import Discretization
+from meshmode.discretization.poly_element import \
+        InterpolatoryQuadratureSimplexGroupFactory
+
+from pytential import bind, sym, norm  # noqa
+
+# {{{ set some constants for use below
+
+nelements = 50
+mesh_order = 4
+target_order = 8
+ovsmp_target_order = 4*target_order
+qbx_order = 4
+fmm_order = 7
+mu = 1
+
+# }}}
+
+
+def main():
+    import logging
+    logging.basicConfig(level=logging.INFO)
+
+    cl_ctx = cl.create_some_context()
+    queue = cl.CommandQueue(cl_ctx)
+
+    from meshmode.mesh.generation import (  # noqa
+            make_curve_mesh, starfish, ellipse, drop)
+    mesh = make_curve_mesh(
+            lambda t: ellipse(1, t),
+            np.linspace(0, 1, nelements+1),
+            target_order)
+    coarse_density_discr = Discretization(
+            cl_ctx, mesh,
+            InterpolatoryQuadratureSimplexGroupFactory(target_order))
+
+    from pytential.qbx import QBXLayerPotentialSource
+    stick_out = 0.05
+    qbx, _ = QBXLayerPotentialSource(
+            coarse_density_discr, fine_order=ovsmp_target_order, qbx_order=qbx_order,
+            fmm_order=fmm_order, target_stick_out_factor=stick_out
+            ).with_refinement()
+
+    density_discr = qbx.density_discr
+    normal = bind(density_discr, sym.normal(2).as_vector())(queue)
+    path_length = bind(density_discr, sym.integral(2, 1, 1))(queue)
+
+    # {{{ describe bvp
+
+    from pytential.symbolic.stokes import StressletWrapper, StokesletWrapper
+    from pytools.obj_array import make_obj_array
+    dim=2
+    cse = sym.cse
+
+    sigma_sym = sym.make_sym_vector("sigma", dim)
+    sqrt_w = sym.sqrt_jac_q_weight(2)
+    inv_sqrt_w_sigma = cse(sigma_sym/sqrt_w)
+    meanless_inv_sqrt_w_sigma = cse(inv_sqrt_w_sigma - sym.mean(2, 1, inv_sqrt_w_sigma))
+    int_inv_sqrt_w_sigma = sym.Ones() * sym.integral(2, 1, inv_sqrt_w_sigma)
+    
+    nvec_sym = sym.make_sym_vector("normal", dim)
+    mu_sym = sym.var("mu")
+
+    # -1 for interior Dirichlet
+    # +1 for exterior Dirichlet
+    loc_sign = 1
+
+    stresslet_obj = StressletWrapper(dim=2)
+    stokeslet_obj = StokesletWrapper(dim=2)
+    bdry_op_sym = -loc_sign * 0.5 * sigma_sym - sqrt_w * (
+                            stresslet_obj.apply(inv_sqrt_w_sigma, nvec_sym, mu_sym, qbx_forced_limit='avg') +
+                            stokeslet_obj.apply(meanless_inv_sqrt_w_sigma, mu_sym, qbx_forced_limit='avg') + 
+                            (0.5/np.pi) * int_inv_sqrt_w_sigma )
+
+    # }}}
+
+    bound_op = bind(qbx, bdry_op_sym)
+
+    # {{{ fix rhs and solve
+
+    def fund_soln(x, y, loc, strength):
+        #with direction (1,0) for point source
+        r = cl.clmath.sqrt((x - loc[0])**2 + (y - loc[1])**2)
+        scaling = strength/(4*np.pi*mu)
+        xcomp = (-cl.clmath.log(r) + (x - loc[0])**2/r**2) * scaling
+        ycomp = ((x - loc[0])*(y - loc[1])/r**2) * scaling
+        return [ xcomp, ycomp ]
+
+    def rotlet_soln(x, y, loc):
+        r = cl.clmath.sqrt((x - loc[0])**2 + (y - loc[1])**2)
+        xcomp = -(y - loc[1])/r**2
+        ycomp = (x - loc[0])/r**2
+        return [ xcomp, ycomp ] 
+
+    def fund_and_rot_soln(x, y, loc, strength):
+        #with direction (1,0) for point source
+        r = cl.clmath.sqrt((x - loc[0])**2 + (y - loc[1])**2)
+        scaling = strength/(4*np.pi*mu)
+        xcomp = (-cl.clmath.log(r) + (x - loc[0])**2/r**2) * scaling - (y - loc[1])*strength*0.125/r**2
+        ycomp = ((x - loc[0])*(y - loc[1])/r**2) * scaling + (x - loc[0])*strength*0.125/r**2
+        return [ xcomp, ycomp ]
+       
+
+    nodes = density_discr.nodes().with_queue(queue)
+    fund_soln_loc = np.array([0.5, -0.2])
+    strength = 100.
+    bc = fund_and_rot_soln(nodes[0], nodes[1], fund_soln_loc, strength)
+ 
+    omega_sym = sym.make_sym_vector("omega", dim)
+    u_A_sym_bdry = stokeslet_obj.apply(omega_sym, mu_sym, qbx_forced_limit=1)
+
+    omega = [cl.array.to_device(queue, (strength/path_length)*np.ones(len(nodes[0]))), cl.array.to_device(queue, np.zeros(len(nodes[0])))]
+    bvp_rhs = bind(qbx, sqrt_w*(sym.make_sym_vector("bc",dim) - u_A_sym_bdry))(queue, bc=bc, mu=mu, omega=omega)
+    from pytential.solve import gmres
+    gmres_result = gmres(
+             bound_op.scipy_op(queue, "sigma", np.float64, mu=mu, normal=normal),
+             bvp_rhs, tol=1e-9, progress=True,
+             stall_iterations=0,
+             hard_failure=True)
+
+    # }}}
+
+
+ # {{{ postprocess/visualize
+    sigma = gmres_result.solution
+    sigma_int_val_sym = sym.make_sym_vector("sigma_int_val", 2)
+    int_val = bind(qbx, sym.integral(2, 1, inv_sqrt_w_sigma))(queue, sigma=sigma)
+    int_val = -int_val/(np.pi)
+    print("int_val = ", int_val)
+
+    u_A_sym_vol = stokeslet_obj.apply(omega_sym, mu_sym, qbx_forced_limit=2)
+    representation_sym = - stresslet_obj.apply(inv_sqrt_w_sigma, nvec_sym, mu_sym, qbx_forced_limit=2) - stokeslet_obj.apply(
+                                meanless_inv_sqrt_w_sigma, mu_sym, qbx_forced_limit=2) + sigma_int_val_sym + u_A_sym_vol
+
+
+    from sumpy.visualization import FieldPlotter
+    nsamp = 30
+    eval_points_1d = np.linspace(-3., 3., nsamp)
+    eval_points = np.zeros((2, len(eval_points_1d)**2))
+    eval_points[0,:] = np.tile(eval_points_1d, len(eval_points_1d))
+    eval_points[1,:] = np.repeat(eval_points_1d, len(eval_points_1d))
+
+    def circle_mask(test_points):
+        return (test_points[0,:]**2 + test_points[1,:]**2 > 1)
+
+    def outside_circle(test_points):
+        mask = circle_mask(test_points)
+        return np.array([
+            row[mask]
+            for row in test_points])
+
+    eval_points = outside_circle(eval_points)
+    from pytential.target import PointsTarget
+    vel = bind(
+            (qbx, PointsTarget(eval_points)),
+            representation_sym)(queue, sigma=sigma, mu=mu, normal=normal, sigma_int_val=int_val, omega=omega)
+    print("@@@@@@@@")
+
+    from sumpy.visualization import FieldPlotter
+    fplot = FieldPlotter(np.zeros(2), extent=6, npoints=100)
+    plot_pts = outside_circle(fplot.points)
+    plot_vel = bind(
+          (qbx, PointsTarget(plot_pts)),
+          representation_sym)(queue, sigma=sigma, mu=mu, normal=normal, sigma_int_val=int_val, omega=omega)
+
+    def get_obj_array(obj_array):
+        from pytools.obj_array import make_obj_array
+        return make_obj_array([
+                ary.get()
+                for ary in obj_array
+                ])
+    exact_soln = fund_and_rot_soln(cl.array.to_device(queue, eval_points[0]), cl.array.to_device(
+                                    queue, eval_points[1]), fund_soln_loc, strength) 
+    vel = get_obj_array(vel)
+    err = vel-get_obj_array(exact_soln)
+    rel_err = err/(get_obj_array(exact_soln))
+
+    print("@@@@@@@@")
+    print("vel[0], err[0], rel_err[0] ***** vel[1], err[1], rel_err[1]: ")
+    for i in range(len(vel[0])):
+        print("%15.8e, %15.8e, %15.8e ***** %15.8e, %15.8e, %15.8e\n"%(
+                        vel[0][i], err[0][i], rel_err[0][i], vel[1][i], err[1][i], rel_err[1][i]))
+
+    print("@@@@@@@@")
+    print("L2 error estimate: ", np.sqrt((6./(nsamp-1))**2*np.sum(err[0]*err[0]) + (6./(nsamp-1))**2*np.sum(err[1]*err[1])))
+    print("L2 rel error estimate: ", np.sqrt((6./(nsamp-1))**2*np.sum(rel_err[0]*rel_err[0]) + (6./(nsamp-1))**2*np.sum(rel_err[1]*rel_err[1])))
+    print("max error at sampled points: ", max(abs(err[0])), max(abs(err[1])))
+    print("max rel error at sampled points: ", max(abs(rel_err[0])), max(abs(rel_err[1])))
+
+    full_pot = np.zeros_like(fplot.points) * float("nan")
+    mask = circle_mask(fplot.points)
+
+    for i, vel in enumerate(plot_vel):
+        full_pot[i,mask] = vel.get()
+
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plt
+    plt.quiver(fplot.points[0], fplot.points[1], full_pot[0], full_pot[1], linewidth=0.1)
+    plt.savefig("exterior-2d-field.pdf")
+
+    # }}}
+
+
+
+if __name__ == "__main__":
+    main()
--- pytential/symbolic/stokes.py
+++ pytential/symbolic/stokes.py
@@ -6,24 +6,25 @@ from sumpy.kernel import StokesletKernel, StressletKernel
 
 
 class StokesletWrapper(object):
-    """ Wrapper class for the Stokeslet kernel.  This class is meant to 
+    """ Wrapper class for the Stokeslet kernel.  This class is meant to
      shield the user from the messiness of writing out every term
-     in the expansion of the double-indiced Stokeslet kernel applied to 
+     in the expansion of the double-indiced Stokeslet kernel applied to
      the density vector.  The object is created
-     to do some of the set-up and bookkeeping once, rather than every 
+     to do some of the set-up and bookkeeping once, rather than every
      time we want to create a symbolic expression based on the kernel -- say,
-     once when we solve for the density, and once when we want a symbolic representation
-     for the solution, for example. 
+     once when we solve for the density, and once when we want a symbolic
+     representation for the solution, for example.
 
-    The apply() function returns the integral expressions needed for 
+    The apply() function returns the integral expressions needed for
      the vector velocity resulting from convolution with the vectory density,
-     and is meant to work similarly to 
-     calling S() (which is IntG()). 
+     and is meant to work similarly to
+     calling S() (which is IntG()).
 
-    .. attribute:: kernel_dict 
-        The dictionary allows us to exploit symmetry -- that StokesletKernel(icomp=0, jcomp=1) is 
-        identical to StokesletKernel(icomp=1, jcomp=0) -- and avoid creating multiple expansions for 
-        the same kernel in a different ordering. 
+    .. attribute:: kernel_dict
+        The dictionary allows us to exploit symmetry -- that
+        StokesletKernel(icomp=0, jcomp=1) is identical to
+        StokesletKernel(icomp=1, jcomp=0) -- and avoid creating multiple expansions for
+        the same kernel in a different ordering.
 
     """
 
@@ -31,39 +32,46 @@ class StokesletWrapper(object):
 
         self.dim = dim
         if dim == 2:
-            self.kernel_dict = { (2, 0) : StokesletKernel(dim=2, icomp=0, jcomp=0),
-                                 (1, 1) : StokesletKernel(dim=2, icomp=0, jcomp=1),
-                                 (0, 2) : StokesletKernel(dim=2, icomp=1, jcomp=1)     }
+            self.kernel_dict = {
+                        (2, 0) : StokesletKernel(dim=2, icomp=0, jcomp=0),
+                        (1, 1) : StokesletKernel(dim=2, icomp=0, jcomp=1),
+                        (0, 2) : StokesletKernel(dim=2, icomp=1, jcomp=1)
+                               }
 
         elif dim == 3:
-            self.kernel_dict = { (2, 0, 0) : StokesletKernel(dim=3, icomp=0, jcomp=0),
-                                 (1, 1, 0) : StokesletKernel(dim=3, icomp=0, jcomp=1),
-                                 (1, 0, 1) : StokesletKernel(dim=3, icomp=0, jcomp=2),
-                                 (0, 2, 0) : StokesletKernel(dim=3, icomp=1, jcomp=1),
-                                 (0, 1, 1) : StokesletKernel(dim=3, icomp=1, jcomp=2),
-                                 (0, 0, 2) : StokesletKernel(dim=3, icomp=2, jcomp=2)  }
+            self.kernel_dict = { 
+                        (2, 0, 0) : StokesletKernel(dim=3, icomp=0, jcomp=0),
+                        (1, 1, 0) : StokesletKernel(dim=3, icomp=0, jcomp=1),
+                        (1, 0, 1) : StokesletKernel(dim=3, icomp=0, jcomp=2),
+                        (0, 2, 0) : StokesletKernel(dim=3, icomp=1, jcomp=1),
+                        (0, 1, 1) : StokesletKernel(dim=3, icomp=1, jcomp=2),
+                        (0, 0, 2) : StokesletKernel(dim=3, icomp=2, jcomp=2)
+                               }
 
 
         else:
-            raise ValueError("unsupported dimension given to StokesletWrapper")             
+            raise ValueError("unsupported dimension given to StokesletWrapper")
 
 
     def apply(self, density_vec_sym, mu_sym, qbx_forced_limit):
-        """ Returns an object array of symbolic expressions for the vector resulting from integrating
-            the dyadic Stokeslet kernel with variable *density_vec_sym*.
+        """ Returns an object array of symbolic expressions for the vector
+            resulting from integrating the dyadic Stokeslet kernel with 
+            variable *density_vec_sym*.
            
             :arg density_vec_sym: a symbolic vector variable for the density vector
             :arg mu_sym: a symbolic variable for the viscosity
-            :arg qbx_forced_limit: the qbx_forced_limit argument to be passed on to IntG.  +/-1 for
-                exterior/interior one-sided boundary limit, +/-2 for exterior/interior off-boundary
-                evaluation, and 'avg' for the average of the two one-sided boundary limits.
+            :arg qbx_forced_limit: the qbx_forced_limit argument to be passed on
+                to IntG.  +/-1 for exterior/interior one-sided boundary limit,
+                +/-2 for exterior/interior off-boundary evaluation, and 'avg'
+                for the average of the two one-sided boundary limits.
         """
 
         sym_expr = np.empty((self.dim,), dtype=object)
 
         for comp in range(self.dim):
 
-            # Start variable count for kernel with 1 for the requested result component
+            # Start variable count for kernel with 1 for the requested result
+            #  component
             base_count = np.zeros(self.dim, dtype=np.int)
             base_count[comp] +=1
 
@@ -73,35 +81,38 @@ class StokesletWrapper(object):
                 ctr_key = tuple(var_ctr)
 
                 if i < 0.1:
-                    sym_expr[comp] = sym.IntG(self.kernel_dict[ctr_key], density_vec_sym[i],
-                                          qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
+                    sym_expr[comp] = sym.IntG(
+                                     self.kernel_dict[ctr_key], density_vec_sym[i],
+                                     qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
 
                 else:
-                    sym_expr[comp] = sym_expr[comp] + sym.IntG(self.kernel_dict[ctr_key], density_vec_sym[i],
-                                                           qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
+                    sym_expr[comp] = sym_expr[comp] + sym.IntG(
+                                     self.kernel_dict[ctr_key], density_vec_sym[i],
+                                     qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
             
 
         return sym_expr
 
 
 class StressletWrapper(object):
-    """ Wrapper class for the Stresslet kernel.  This class is meant to 
+    """ Wrapper class for the Stresslet kernel.  This class is meant to
      shield the user from the messiness of writing out every term
      in the expansion of the triple-indiced Stresslet kernel applied to both
      a normal vector and the density vector.  The object is created
-     to do some of the set-up and bookkeeping once, rather than every 
+     to do some of the set-up and bookkeeping once, rather than every
      time we want to create a symbolic expression based on the kernel -- say,
-     once when we solve for the density, and once when we want a symbolic representation
-     for the solution, for example. 
+     once when we solve for the density, and once when we want a symbolic
+     representation for the solution, for example.
 
     The apply() function returns the integral expressions needed for convolving
      the kernel with a vectory density, and is meant to work similarly to
-     calling S() (which is IntG()). 
+     calling S() (which is IntG()).
 
-    .. attribute:: kernel_dict 
-        The dictionary allows us to exploit symmetry -- that StressletKernel(icomp=0, jcomp=1, kcomp=2) is 
-        identical to StressletKernel(icomp=1, jcomp=2, kcomp=0) -- and avoid creating multiple expansions for 
-        the same kernel in a different ordering. 
+    .. attribute:: kernel_dict
+        The dictionary allows us to exploit symmetry -- that
+        StressletKernel(icomp=0, jcomp=1, kcomp=2) is identical to
+        StressletKernel(icomp=1, jcomp=2, kcomp=0) -- and avoid creating
+        multiple expansions for the same kernel in a different ordering.
 
     """
 
@@ -109,22 +120,26 @@ class StressletWrapper(object):
 
         self.dim = dim
         if dim == 2:
-            self.kernel_dict = { (3, 0) : StressletKernel(dim=2, icomp=0, jcomp=0, kcomp=0),
-                                 (2, 1) : StressletKernel(dim=2, icomp=0, jcomp=0, kcomp=1),
-                                 (1, 2) : StressletKernel(dim=2, icomp=0, jcomp=1, kcomp=1),
-                                 (0, 3) : StressletKernel(dim=2, icomp=1, jcomp=1, kcomp=1)    }
+            self.kernel_dict = {
+                (3, 0) : StressletKernel(dim=2, icomp=0, jcomp=0, kcomp=0),
+                (2, 1) : StressletKernel(dim=2, icomp=0, jcomp=0, kcomp=1),
+                (1, 2) : StressletKernel(dim=2, icomp=0, jcomp=1, kcomp=1),
+                (0, 3) : StressletKernel(dim=2, icomp=1, jcomp=1, kcomp=1)
+                               }
 
         elif dim == 3:
-            self.kernel_dict = { (3, 0, 0) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=0),
-                                 (2, 1, 0) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=1),
-                                 (2, 0, 1) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=2),
-                                 (1, 2, 0) : StressletKernel(dim=3, icomp=0, jcomp=1, kcomp=1),
-                                 (1, 1, 1) : StressletKernel(dim=3, icomp=0, jcomp=1, kcomp=2),
-                                 (1, 0, 2) : StressletKernel(dim=3, icomp=0, jcomp=2, kcomp=2),
-                                 (0, 3, 0) : StressletKernel(dim=3, icomp=1, jcomp=1, kcomp=1),
-                                 (0, 2, 1) : StressletKernel(dim=3, icomp=1, jcomp=1, kcomp=2),
-                                 (0, 1, 2) : StressletKernel(dim=3, icomp=1, jcomp=2, kcomp=2),
-                                 (0, 0, 3) : StressletKernel(dim=3, icomp=2, jcomp=2, kcomp=2)  }
+            self.kernel_dict = {
+                (3, 0, 0) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=0),
+                (2, 1, 0) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=1),
+                (2, 0, 1) : StressletKernel(dim=3, icomp=0, jcomp=0, kcomp=2),
+                (1, 2, 0) : StressletKernel(dim=3, icomp=0, jcomp=1, kcomp=1),
+                (1, 1, 1) : StressletKernel(dim=3, icomp=0, jcomp=1, kcomp=2),
+                (1, 0, 2) : StressletKernel(dim=3, icomp=0, jcomp=2, kcomp=2),
+                (0, 3, 0) : StressletKernel(dim=3, icomp=1, jcomp=1, kcomp=1),
+                (0, 2, 1) : StressletKernel(dim=3, icomp=1, jcomp=1, kcomp=2),
+                (0, 1, 2) : StressletKernel(dim=3, icomp=1, jcomp=2, kcomp=2),
+                (0, 0, 3) : StressletKernel(dim=3, icomp=2, jcomp=2, kcomp=2)
+                               }
 
 
         else:
@@ -132,16 +147,18 @@ class StressletWrapper(object):
 
 
     def apply(self, density_vec_sym, dir_vec_sym, mu_sym, qbx_forced_limit):
-        """ Returns an object array of symbolic expressions for the vector resulting from integrating
-            the dyadic Stresslet kernel with direction vector variable name *dir_vec_sym* and
+        """ Returns an object array of symbolic expressions for the vector
+            resulting from integrating the dyadic Stresslet kernel with
+            direction vector variable name *dir_vec_sym* and
             variable *density_vec_sym*.
-           
+
             :arg density_vec_sym: a symbolic vector variable for the density vector
-            :arg dir_vec_sym: a symbolic vector variable for the direction vector 
+            :arg dir_vec_sym: a symbolic vector variable for the direction vector
             :arg mu_sym: a symbolic variable for the viscosity
-            :arg qbx_forced_limit: the qbx_forced_limit argument to be passed on to IntG.  +/-1 for
-                exterior/interior one-sided boundary limit, +/-2 for exterior/interior off-boundary
-                evaluation, and 'avg' for the average of the two one-sided boundary limits.
+            :arg qbx_forced_limit: the qbx_forced_limit argument to be passed
+                on to IntG.  +/-1 for exterior/interior one-sided boundary limit,
+                +/-2 for exterior/interior off-boundary evaluation, and 'avg'
+                for the average of the two one-sided boundary limits.
         """
 
         import itertools
@@ -150,7 +167,8 @@ class StressletWrapper(object):
 
         for comp in range(self.dim):
 
-            # Start variable count for kernel with 1 for the requested result component
+            # Start variable count for kernel with 1 for the requested result
+            #   component
             base_count = np.zeros(self.dim, dtype=np.int)
             base_count[comp] +=1
 
@@ -161,13 +179,18 @@ class StressletWrapper(object):
                 ctr_key = tuple(var_ctr)
 
                 if i + j < 0.1:
-                    sym_expr[comp] = sym.IntG(self.kernel_dict[ctr_key], dir_vec_sym[i] * density_vec_sym[j],
-                                          qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
+                    sym_expr[comp] = sym.IntG(
+                                     self.kernel_dict[ctr_key],
+                                     dir_vec_sym[i] * density_vec_sym[j],
+                                     qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
 
                 else:
-                    sym_expr[comp] = sym_expr[comp] + sym.IntG(self.kernel_dict[ctr_key], dir_vec_sym[i] * density_vec_sym[j],
-                                                           qbx_forced_limit=qbx_forced_limit, mu=mu_sym)
-            
+                    sym_expr[comp] = sym_expr[comp] + sym.IntG(
+                                                   self.kernel_dict[ctr_key],
+                                                   dir_vec_sym[i] * density_vec_sym[j],
+                                                   qbx_forced_limit=qbx_forced_limit,
+                                                   mu=mu_sym)
+
 
         return sym_expr
 
