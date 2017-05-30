ed5bbe8b30deb04e840ac4eb77e5bdde7eb739ab
Andreas Kloeckner <inform@tiker.net>
2017-05-30 01:29:53 -0400

    Add prototype homogeneous C-H solver

---
+++ examples/cahn-hilliard.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.clmath  # noqa
+
+from meshmode.discretization import Discretization
+from meshmode.discretization.poly_element import \
+        InterpolatoryQuadratureSimplexGroupFactory
+
+from pytential import bind, sym, norm  # noqa
+from pytential.target import PointsTarget
+
+# {{{ set some constants for use below
+
+nelements = 20
+bdry_quad_order = 4
+mesh_order = bdry_quad_order
+qbx_order = bdry_quad_order
+bdry_ovsmp_quad_order = 4*bdry_quad_order
+fmm_order = 8
+
+# }}}
+
+
+def main():
+    import logging
+    logging.basicConfig(level=logging.INFO)
+
+    cl_ctx = cl.create_some_context()
+    queue = cl.CommandQueue(cl_ctx)
+
+    from meshmode.mesh.generation import ellipse, make_curve_mesh
+    from functools import partial
+
+    mesh = make_curve_mesh(
+                partial(ellipse, 2),
+                np.linspace(0, 1, nelements+1),
+                mesh_order)
+
+    pre_density_discr = Discretization(
+            cl_ctx, mesh,
+            InterpolatoryQuadratureSimplexGroupFactory(bdry_quad_order))
+
+    from pytential.qbx import (
+            QBXLayerPotentialSource, QBXTargetAssociationFailedException)
+    qbx, _ = QBXLayerPotentialSource(
+            pre_density_discr, fine_order=bdry_ovsmp_quad_order, qbx_order=qbx_order,
+            fmm_order=fmm_order,
+            expansion_disks_in_tree_have_extent=True,
+            ).with_refinement()
+    density_discr = qbx.density_discr
+
+    from pytential.symbolic.pde.cahn_hilliard import CahnHilliardOperator
+    chop = CahnHilliardOperator(
+            # FIXME: Constants?
+            lambda1=1.5,
+            lambda2=1.25,
+            c=1)
+
+    unk = chop.make_unknown("sigma")
+    bound_op = bind(qbx, chop.operator(unk))
+
+    # {{{ fix rhs and solve
+
+    nodes = density_discr.nodes().with_queue(queue)
+
+    def g(xvec):
+        x, y = xvec
+        return cl.clmath.atan2(y, x)
+
+    bc = sym.make_obj_array([
+        # FIXME: Realistic BC
+        g(nodes),
+        -g(nodes),
+        ])
+
+    from pytential.solve import gmres
+    gmres_result = gmres(
+            bound_op.scipy_op(queue, "sigma", dtype=np.complex128),
+            bc, tol=1e-8, progress=True,
+            stall_iterations=0,
+            hard_failure=True)
+
+    # }}}
+
+    # {{{ postprocess/visualize
+
+    sigma = gmres_result.solution
+
+    from sumpy.visualization import FieldPlotter
+    fplot = FieldPlotter(np.zeros(2), extent=5, npoints=500)
+
+    targets = cl.array.to_device(queue, fplot.points)
+
+    qbx_stick_out = qbx.copy(target_stick_out_factor=0.05)
+
+    indicator_qbx = qbx_stick_out.copy(qbx_order=2)
+
+    from sumpy.kernel import LaplaceKernel
+    ones_density = density_discr.zeros(queue)
+    ones_density.fill(1)
+    indicator = bind(
+            (indicator_qbx, PointsTarget(targets)),
+            sym.D(LaplaceKernel(2), sym.var("sigma")))(
+            queue, sigma=ones_density).get()
+
+    try:
+        fld_in_vol = bind(
+                (qbx_stick_out, PointsTarget(targets)),
+                chop.representation(unk))(queue, sigma=sigma).get()
+    except QBXTargetAssociationFailedException as e:
+        fplot.write_vtk_file(
+                "failed-targets.vts",
+                [
+                    ("failed", e.failed_target_flags.get(queue))
+                    ]
+                )
+        raise
+
+    #fplot.show_scalar_in_mayavi(fld_in_vol.real, max_val=5)
+    fplot.write_vtk_file(
+            "potential.vts",
+            [
+                ("potential", fld_in_vol),
+                ("indicator", indicator),
+                ]
+            )
+
+    # }}}
+
+
+if __name__ == "__main__":
+    main()
---
+++ pytential/symbolic/pde/cahn_hilliard.py
@@ -0,0 +1,106 @@
+from __future__ import division, absolute_import, print_function
+
+__copyright__ = "Copyright (C) 2017 Andreas Kloeckner"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+
+__doc__ = """
+.. autoclass:: CahnHilliardOperator
+"""
+
+import numpy as np
+from pytential.symbolic.pde.scalar import L2WeightedPDEOperator
+from pytential import sym
+from functools import partial
+
+
+class CahnHilliardOperator(L2WeightedPDEOperator):
+    def __init__(self, lambda1, lambda2, c):
+        self.lambdas = (lambda1, lambda2)
+        self.c = c
+
+    def make_unknown(self, name):
+        return sym.make_sym_vector(name, 2)
+
+    def S_G(self, i, density, qbx_forced_limit, op_map=None):  # noqa: N802
+        if op_map is None:
+            op_map = lambda x: x  # noqa: E731
+
+        from sumpy.kernel import HelmholtzKernel
+        hhk = HelmholtzKernel(2, allow_evanescent=True)
+        hhk_scaling = 1j/4
+
+        if i == 0:
+            lam1, lam2 = self.lambdas
+            return (
+                    # FIXME: Verify scaling
+                    -1/(2*np.pi*(lam1**2-lam2**2)) / hhk_scaling
+                    *
+                    (
+                        op_map(sym.S(hhk, density, k=1j*lam1,
+                            qbx_forced_limit=qbx_forced_limit))
+                        -
+                        op_map(sym.S(hhk, density, k=1j*lam2,
+                            qbx_forced_limit=qbx_forced_limit))))
+        else:
+            return (
+                    # FIXME: Verify scaling
+
+                    -1/(2*np.pi) / hhk_scaling
+                    * op_map(sym.S(hhk, density, k=1j*self.lambdas[i-1],
+                        qbx_forced_limit=qbx_forced_limit)))
+
+    def representation(self, unknown):
+        sig1, sig2 = unknown
+        S_G = partial(self.S_G, qbx_forced_limit=None)  # noqa: N806
+
+        return S_G(1, sig1) + S_G(0, sig2)
+
+    def operator(self, unknown):
+        sig1, sig2 = unknown
+        lam1, lam2 = self.lambdas
+        S_G = partial(self.S_G, qbx_forced_limit=1)  # noqa: N806
+
+        c = self.c
+
+        def Sn_G(i, density):  # noqa
+            return self.S_G(i, density,
+                        qbx_forced_limit="avg",
+                        op_map=partial(sym.normal_derivative, 2))
+
+        d = sym.make_obj_array([
+            0.5*sig1,
+            0.5*lam2**2*sig1 - 0.5*sig2
+            ])
+        a = sym.make_obj_array([
+            # A11
+            Sn_G(1, sig1) + c*S_G(1, sig1)
+            # A12
+            + Sn_G(0, sig2) + c*S_G(0, sig2),
+
+            # A21
+            lam2**2*Sn_G(1, sig1)
+            # A22
+            - Sn_G(1, sig2) + lam1**2*Sn_G(0, sig2)
+            ])
+
+        return d+a
