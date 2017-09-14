f76a30ee231168d580522b017b88103311ed39a9
Andreas Kloeckner <inform@tiker.net>
2017-09-14 14:27:26 -0500

    Fix up layer potential examples

--- examples/layerpot-3d.py
+++ examples/layerpot-3d.py
@@ -17,22 +17,27 @@ cl_ctx = cl.create_some_context()
 queue = cl.CommandQueue(cl_ctx)
 
 target_order = 5
-qbx_order = 2
+qbx_order = 3
 nelements = 60
 mode_nr = 0
 
-h = 0.4
+if 1:
+    cad_file_name = "ellipsoid.step"
+    h = 0.6
+else:
+    cad_file_name = "two-cylinders-smooth.step"
+    h = 0.4
 
 k = 0
 if k:
-    kernel = HelmholtzKernel("k")
+    kernel = HelmholtzKernel(3)
 else:
-    kernel = LaplaceKernel()
+    kernel = LaplaceKernel(3)
 #kernel = OneKernel()
 
 from meshmode.mesh.io import generate_gmsh, FileSource
 mesh = generate_gmsh(
-        FileSource("molecule.step"), 2, order=2,
+        FileSource(cad_file_name), 2, order=2,
         other_options=["-string", "Mesh.CharacteristicLengthMax = %g;" % h])
 
 from meshmode.mesh.processing import find_bounding_box
@@ -51,16 +56,17 @@ density_discr = Discretization(
         cl_ctx, mesh, InterpolatoryQuadratureSimplexGroupFactory(target_order))
 
 qbx = QBXLayerPotentialSource(density_discr, 4*target_order, qbx_order,
-        fmm_order=qbx_order)
+        fmm_order=qbx_order + 3,
+        target_association_tolerance=0.15)
 
 nodes = density_discr.nodes().with_queue(queue)
 
 angle = cl.clmath.atan2(nodes[1], nodes[0])
 
 from pytential import bind, sym
-#op = sym.d_dx(sym.S(kernel, sym.var("sigma")))
-op = sym.D(kernel, sym.var("sigma"))
-#op = sym.S(kernel, sym.var("sigma"))
+#op = sym.d_dx(sym.S(kernel, sym.var("sigma"), qbx_forced_limit=None))
+op = sym.D(kernel, sym.var("sigma"), qbx_forced_limit=None)
+#op = sym.S(kernel, sym.var("sigma"), qbx_forced_limit=None)
 
 sigma = cl.clmath.cos(mode_nr*angle)
 if 0:
@@ -72,32 +78,29 @@ if 0:
 if isinstance(kernel, HelmholtzKernel):
     sigma = sigma.astype(np.complex128)
 
-bound_bdry_op = bind(qbx, op)
-#mlab.figure(bgcolor=(1, 1, 1))
-if 1:
-    fplot = FieldPlotter(bbox_center, extent=1.5*bbox_size, npoints=150)
+fplot = FieldPlotter(bbox_center, extent=1.5*bbox_size, npoints=150)
 
-    from pytential.target import PointsTarget
-    fld_in_vol = bind(
-            (qbx, PointsTarget(fplot.points)),
-            op)(queue, sigma=sigma, k=k).get()
+from pytential.target import PointsTarget
+fld_in_vol = bind(
+        (qbx, PointsTarget(fplot.points)),
+        op)(queue, sigma=sigma, k=k).get()
 
-    #fplot.show_scalar_in_mayavi(fld_in_vol.real, max_val=5)
-    fplot.write_vtk_file(
-            "potential.vts",
-            [
-                ("potential", fld_in_vol)
-                ]
-            )
+#fplot.show_scalar_in_mayavi(fld_in_vol.real, max_val=5)
+fplot.write_vtk_file(
+        "potential.vts",
+        [
+            ("potential", fld_in_vol)
+            ]
+        )
 
-    bdry_normals = bind(
-            density_discr,
-            sym.normal(density_discr.ambient_dim))(queue).as_vector(dtype=object)
+bdry_normals = bind(
+        density_discr,
+        sym.normal(density_discr.ambient_dim))(queue).as_vector(dtype=object)
 
-    from meshmode.discretization.visualization import make_visualizer
-    bdry_vis = make_visualizer(queue, density_discr, target_order)
+from meshmode.discretization.visualization import make_visualizer
+bdry_vis = make_visualizer(queue, density_discr, target_order)
 
-    bdry_vis.write_vtk_file("source.vtu", [
-        ("sigma", sigma),
-        ("bdry_normals", bdry_normals),
-        ])
+bdry_vis.write_vtk_file("source.vtu", [
+    ("sigma", sigma),
+    ("bdry_normals", bdry_normals),
+    ])
