4988baea0298ce6e894a133e692793a6d9bedfa3
Matt Wala <wala1@illinois.edu>
2017-10-11 02:53:43 -0500

    Apply pytest.skip() to the correct test.

--- test/test_layer_pot_identity.py
+++ test/test_layer_pot_identity.py
@@ -40,6 +40,7 @@ from meshmode.mesh.generation import (  # noqa
 from pytential import bind, sym, norm
 from sumpy.kernel import LaplaceKernel, HelmholtzKernel
 
+
 import logging
 logger = logging.getLogger(__name__)
 
