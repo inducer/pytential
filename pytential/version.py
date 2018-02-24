2fcce814b808266c0e57ae0884993bbafb12415b
Andreas Klöckner <inform@tiker.net>
2018-02-24 10:34:55 -0500

    Bump kernel version, back to master

--- pytential/version.py
+++ pytential/version.py
@@ -5,4 +5,4 @@ VERSION_TEXT = ".".join(str(i) for i in VERSION)
 # branch name, so as to avoid conflicts with the master branch. Make sure
 # to reset this to the next number up with "master" before merging into
 # master.
-PYTENTIAL_KERNEL_VERSION = ("remove-nosync", 8)
+PYTENTIAL_KERNEL_VERSION = ("master", 9)
