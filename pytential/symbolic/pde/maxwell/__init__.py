49ac891c0ce5170f38c190c6790aae6497820462
Andreas Kloeckner <inform@tiker.net>
2018-07-02 12:50:28 -0500

    Clean up non-test Flake8 complaints

--- pytential/symbolic/pde/maxwell/__init__.py
+++ pytential/symbolic/pde/maxwell/__init__.py
@@ -117,7 +117,8 @@ def get_sym_maxwell_plane_wave(amplitude_vec, v, omega, epsilon=1, mu=1, where=N
 
 # }}}
 
-# {{{ point source for vector potential based on Lorenz gauge 
+
+# {{{ Maxwell sources for scalar/vector potentials
 
 def get_sym_maxwell_point_source_potentials(kernel, jxyz, k):
     r"""Return a symbolic expression that, when bound to a
@@ -136,50 +137,67 @@ def get_sym_maxwell_point_source_potentials(kernel, jxyz, k):
         field[:3]/(1j*k)    # vector potential
         )
 
-# }}}
 
 def get_sym_maxwell_planewave_gradphi(u, Ep, k, where=None):
-    r"""
-    Return symbolic expression that can be bound to a :class:`pytential.source.PointPotentialSource`
-    and yield the gradient of a scalar potential field satisfying Maxwell's equations.
+    r""" Return symbolic expression that can be bound to a
+    :class:`pytential.source.PointPotentialSource` and yield the gradient of a
+    scalar potential field satisfying Maxwell's equations.
 
-    Should be representing the following:
+    Represents the following:
+
     .. math::
+
         \nabla \phi(x) = - e^{i k x^T u} E_p^T \left( 1 + i k x^T u\right)
     """
     x = sym.nodes(3, where).as_vector()
-    grad_phi = -sym.exp(1j*k*np.dot(x,u)) * (Ep.T + 1j*k*np.dot(Ep,x)*u.T)
+    grad_phi = -sym.exp(1j*k*np.dot(x, u)) * (Ep.T + 1j*k*np.dot(Ep, x)*u.T)
     return grad_phi
 
-def get_sym_maxwell_planewave_divA(u, Ep, k, epsilon=1, mu=1, where=None):
-    r"""
-    Return symbolic expression that can be bound to a :class:`pytential.source.PointPotentialSource`
-    and yield the divergence of a vector potential field satisfying Maxwell's equations.
 
-    Should be representing the following:
+def get_sym_maxwell_planewave_divA(u, Ep, k, epsilon=1, mu=1, where=None):
+    r"""Return symbolic expression that can be bound to a
+    :class:`pytential.source.PointPotentialSource` and yield the divergence of
+    a vector potential field satisfying Maxwell's equations.
+
+    Represents the following:
+
     .. math::
-        \nabla \cdot \boldsymbol{A} = -\sqrt{\mu \epsilon} e^{i k x^T u} E_p^T \left( u + i k x\right)
+
+        \nabla \cdot \boldsymbol{A} = -\sqrt{\mu \epsilon}
+        e^{i k x^T u} E_p^T \left( u + i k x\right)
     """
     x = sym.nodes(3, where).as_vector()
-    divA = sym.join_fields(-sym.sqrt(epsilon*mu) * sym.exp(1j*k*np.dot(x,u)) * np.dot(Ep,u + 1j*k*x))
+    divA = sym.join_fields(
+            -sym.sqrt(epsilon*mu) * sym.exp(1j*k*np.dot(x, u))
+            * np.dot(Ep, u + 1j*k*x))
+
     return divA
 
-def get_sym_maxwell_planewave_potentials(u, Ep, k, epsilon=1, mu=1, where=None):
-    r"""
-    Return a 2-tuple of symbolic expressions that can be bound to a :class:`pytential.source.PointPotentialSource`
-    and yield the scalar and vector potential fields satisfying Maxwell's equations that represent
-    a plane wave.
 
-    Should be representing the following:
+def get_sym_maxwell_planewave_potentials(u, Ep, k, epsilon=1, mu=1, where=None):
+    r"""Return a 2-tuple of symbolic expressions that can be bound to a
+    :class:`pytential.source.PointPotentialSource` and yield the scalar and
+    vector potential fields satisfying Maxwell's equations that represent a
+    plane wave.
+
+    Represents the following:
+
     .. math::
-        \boldsymbol{A} = -u \left(x \cdot E_p \right)\sqrt{\mu \epsilon} e^{i k x^T u}
+
+        \boldsymbol{A} = -u \left(x \cdot E_p \right)
+        \sqrt{\mu \epsilon} e^{i k x^T u}
+
     .. math::
+
         \phi = - \left(x \cdot E_p\right) e^{i k x^T u}
     """
     x = sym.nodes(3, where).as_vector()
-    A = -u * np.dot(x,Ep) * sym.sqrt(epsilon*mu) * sym.exp(1j*k*np.dot(x,u))
-    phi = sym.join_fields(-np.dot(x,Ep) * sym.exp(1j*k*np.dot(x,u)))
-    return (phi, A)
+    A = -u * np.dot(x, Ep) * sym.sqrt(epsilon*mu) * sym.exp(1j*k*np.dot(x, u))
+    phi = sym.join_fields(-np.dot(x, Ep) * sym.exp(1j*k*np.dot(x, u)))
+    return phi, A
+
+# }}}
+
 
 # {{{ Charge-Current MFIE
 
