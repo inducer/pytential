40f696c9069cc59552d19f2920883a38c0bf3e23
Alexandru Fikl <alexfikl@gmail.com>
2019-07-21 10:27:45 -0500

    source: copy instead of multiplying by 1

--- pytential/source.py
+++ pytential/source.py
@@ -130,8 +130,7 @@ class PointPotentialSource(PotentialSource):
         for arg_name, arg_expr in six.iteritems(insn.kernel_arguments):
             kernel_args[arg_name] = evaluate(arg_expr)
 
-        strengths = (evaluate(insn.density).with_queue(queue)
-                * self.weights_and_area_elements())
+        strengths = evaluate(insn.density).with_queue(queue).copy()
 
         # FIXME: Do this all at once
         result = []
@@ -203,6 +202,9 @@ class LayerPotentialSourceBase(PotentialSource):
 
     @property
     def resampler(self):
+        raise NotImplementedError
+
+    def with_refinement(self):
         raise NotImplementedError
 
     @property
