ce708f8a8f6c31ad3877977ee7ec8c34088a41d1
Andreas Kloeckner <inform@tiker.net>
2019-08-06 13:45:48 -0500

    Better doc reference

--- pytential/qbx/cost.py
+++ pytential/qbx/cost.py
@@ -274,7 +274,7 @@ class CostModel(object):
         :arg use_target_specific_qbx: A :class:`bool` indicating whether
             target-specific QBX is used
         :arg translation_cost_model_factory: A callable which, given arguments
-            (*dim*, *nlevels*), returns a translation cost model.
+            (*dim*, *nlevels*), returns a :class:`TranslationCostModel`.
         """
         self.translation_cost_model_factory = translation_cost_model_factory
         if calibration_params is None:
