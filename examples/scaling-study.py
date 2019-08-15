553d0593c89d7c4bbf601553af4aff6300237107
Alexandru Fikl <alexfikl@gmail.com>
2019-08-15 15:59:23 -0500

    cache in examples/scaling-study

--- examples/scaling-study.py
+++ examples/scaling-study.py
@@ -6,7 +6,7 @@ from meshmode.discretization import Discretization
 from meshmode.discretization.poly_element import \
         InterpolatoryQuadratureSimplexGroupFactory
 
-from pytential import bind, sym, norm  # noqa
+from pytential import bind, sym
 from pytential.target import PointsTarget
 
 # {{{ set some constants for use below
@@ -22,7 +22,7 @@ k = 0
 # }}}
 
 
-def make_mesh(nx, ny):
+def make_mesh(nx, ny, visualize=False):
     from meshmode.mesh.generation import ellipse, make_curve_mesh
     from functools import partial
 
@@ -43,7 +43,7 @@ def make_mesh(nx, ny):
 
     mesh = merge_disjoint_meshes(meshes, single_group=True)
 
-    if 0:
+    if visualize:
         from meshmode.mesh.visualization import draw_curve
         draw_curve(mesh)
         import matplotlib.pyplot as plt
@@ -52,14 +52,14 @@ def make_mesh(nx, ny):
     return mesh
 
 
-def timing_run(nx, ny):
+def timing_run(nx, ny, visualize=False):
     import logging
     logging.basicConfig(level=logging.WARNING)  # INFO for more progress info
 
     cl_ctx = cl.create_some_context()
     queue = cl.CommandQueue(cl_ctx)
 
-    mesh = make_mesh(nx=nx, ny=ny)
+    mesh = make_mesh(nx=nx, ny=ny, visualize=visualize)
 
     density_discr = Discretization(
             cl_ctx, mesh,
@@ -71,6 +71,32 @@ def timing_run(nx, ny):
             density_discr, fine_order=bdry_ovsmp_quad_order, qbx_order=qbx_order,
             fmm_order=fmm_order
             )
+
+    places = {}
+    if visualize:
+        from sumpy.visualization import FieldPlotter
+        fplot = FieldPlotter(np.zeros(2), extent=5, npoints=1500)
+
+        targets = PointsTarget(cl.array.to_device(queue, fplot.points))
+        qbx_tgt_tol = qbx.copy(target_association_tolerance=0.05)
+        qbx_tgt_indicator = qbx_tgt_tol.copy(
+                fmm_level_to_order=lambda lev: 7,
+                qbx_order=2)
+        qbx_stick_out = qbx.copy(target_stick_out_factor=0.1)
+
+        places.update({
+            "plot-targets": targets,
+            "qbx-target-tol": qbx_tgt_tol,
+            "qbx-indicator": qbx_tgt_indicator,
+            "qbx-stick-out": qbx_stick_out
+            })
+    places.update({
+        sym.DEFAULT_SOURCE: qbx,
+        sym.DEFAULT_TARGET: qbx.density_discr
+        })
+
+    from pytential.symbolic.execution import GeometryCollection
+    places = GeometryCollection(places)
 
     # {{{ describe bvp
 
@@ -90,15 +116,14 @@ def timing_run(nx, ny):
     # +1 for exterior Dirichlet
     loc_sign = +1
 
-    bdry_op_sym = (-loc_sign*0.5*sigma_sym
-            + sqrt_w*(
-                alpha*sym.S(kernel, inv_sqrt_w_sigma, k=sym.var("k"))
-                - sym.D(kernel, inv_sqrt_w_sigma, k=sym.var("k"))
-                ))
+    k_sym = sym.var("k")
+    S_sym = sym.S(kernel, inv_sqrt_w_sigma, k=k, qbx_forced_limit=+1)
+    D_sym = sym.D(kernel, inv_sqrt_w_sigma, k=k, qbx_forced_limit="avg")
+    bdry_op_sym = -loc_sign*0.5*sigma_sym + sqrt_w*(alpha*S_sym + D_sym)
 
     # }}}
 
-    bound_op = bind(qbx, bdry_op_sym)
+    bound_op = bind(places, bdry_op_sym)
 
     # {{{ fix rhs and solve
 
@@ -115,50 +140,36 @@ def timing_run(nx, ny):
     repr_kwargs = dict(k=sym.var("k"), qbx_forced_limit=+1)
 
     sym_op = sym.S(kernel, sym.var("sigma"), **repr_kwargs)
-    bound_op = bind(qbx, sym_op)
+    bound_op = bind(places, sym_op)
 
     print("FMM WARM-UP RUN 1: %d elements" % mesh.nelements)
     bound_op(queue, sigma=sigma, k=k)
     print("FMM WARM-UP RUN 2: %d elements" % mesh.nelements)
     bound_op(queue, sigma=sigma, k=k)
     queue.finish()
-    print("FMM TIMING RUN: %d elements" % mesh.nelements)
+    print("FMM TIMING RUN:    %d elements" % mesh.nelements)
 
     from time import time
     t_start = time()
-
     bound_op(queue, sigma=sigma, k=k)
     queue.finish()
-    elapsed = time()-t_start
+    t_end = time()
 
+    elapsed = t_end - t_start
     print("FMM TIMING RUN DONE: %d elements -> %g s"
             % (mesh.nelements, elapsed))
 
-    return (mesh.nelements, elapsed)
-
-    if 0:
-        from sumpy.visualization import FieldPlotter
-        fplot = FieldPlotter(np.zeros(2), extent=5, npoints=1500)
-
-        targets = cl.array.to_device(queue, fplot.points)
-
-        qbx_tgt_tol = qbx.copy(target_association_tolerance=0.05)
-
-        indicator_qbx = qbx_tgt_tol.copy(
-                fmm_level_to_order=lambda lev: 7, qbx_order=2)
-
+    if visualize:
         ones_density = density_discr.zeros(queue)
         ones_density.fill(1)
-        indicator = bind(
-                (indicator_qbx, PointsTarget(targets)),
-                sym_op)(
+        indicator = bind(places, sym_op,
+                auto_where=("qbx-indicator", "plot-targets"))(
                 queue, sigma=ones_density).get()
 
-        qbx_stick_out = qbx.copy(target_stick_out_factor=0.1)
         try:
-            fld_in_vol = bind(
-                    (qbx_stick_out, PointsTarget(targets)),
-                    sym_op)(queue, sigma=sigma, k=k).get()
+            fld_in_vol = bind(places, sym_op,
+                    auto_where=("qbx-stick-out", "plot_targets"))(
+                    queue, sigma=sigma, k=k).get()
         except QBXTargetAssociationFailedException as e:
             fplot.write_vtk_file(
                     "failed-targets.vts",
@@ -168,7 +179,6 @@ def timing_run(nx, ny):
                     )
             raise
 
-        #fplot.show_scalar_in_mayavi(fld_in_vol.real, max_val=5)
         fplot.write_vtk_file(
                 "potential-scaling.vts",
                 [
@@ -177,12 +187,13 @@ def timing_run(nx, ny):
                     ]
                 )
 
+    return (mesh.nelements, elapsed)
+
     # }}}
 
 
 if __name__ == "__main__":
-    results = []
-    for nx, ny in [
+    grid_sizes = [
             (3, 3),
             (3, 4),
             (4, 4),
@@ -198,9 +209,14 @@ if __name__ == "__main__":
             (9, 9),
             (9, 10),
             (10, 10),
-            ]:
+            ]
 
-        results.append(timing_run(nx, ny))
+    from pytools.convergence import EOCRecorder
+    eoc = EOCRecorder()
 
-    for r in results:
-        print(r)
+    for k, (nx, ny) in enumerate(grid_sizes):
+        npoints, t_elapsed = timing_run(nx, ny)
+        eoc.add_data_point(npoints, t_elapsed)
+    print(eoc.pretty_print(
+        abscissa_label='Elements',
+        error_label='Timing (s)'))
