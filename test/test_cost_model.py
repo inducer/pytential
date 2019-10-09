a9a044b543096840d5d0634a313bfe7d9c91fb79
Hao Gao <gaohao95@gmail.com>
2019-10-09 17:07:25 -0500

    Add more cost model test cases

--- test/test_cost_model.py
+++ test/test_cost_model.py
@@ -31,10 +31,15 @@ from pyopencl.tools import (  # noqa
 
 import numpy as np
 import pyopencl as cl
+
 from pytential.qbx import QBXLayerPotentialSource
+from sumpy.kernel import LaplaceKernel
+from pytential import bind, sym, norm  # noqa
+
 from pytential.qbx.cost import (
     CLQBXCostModel, PythonQBXCostModel, pde_aware_translation_cost_model
 )
+
 import time
 
 import logging
@@ -44,7 +49,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@pytest.mark.opencl
+# {{{ Compare the time and result of OpenCL implementation and Python implementation
+
 def test_compare_cl_and_py_cost_model(ctx_factory):
     nelements = 3600
     target_order = 16
@@ -257,9 +263,144 @@ def test_compare_cl_and_py_cost_model(ctx_factory):
 
     # }}}
 
+# }}}
+
+
+# {{{ global params
+
+TARGET_ORDER = 8
+OVSMP_FACTOR = 5
+TCF = 0.9
+QBX_ORDER = 5
+FMM_ORDER = 10
+
+DEFAULT_LPOT_KWARGS = {
+        "_box_extent_norm": "l2",
+        "_from_sep_smaller_crit": "static_l2",
+        }
+
+
+def get_lpot_source(queue, dim):
+    from meshmode.discretization import Discretization
+    from meshmode.discretization.poly_element import (
+            InterpolatoryQuadratureSimplexGroupFactory)
+
+    target_order = TARGET_ORDER
+
+    if dim == 2:
+        from meshmode.mesh.generation import starfish, make_curve_mesh
+        mesh = make_curve_mesh(starfish, np.linspace(0, 1, 50), order=target_order)
+    elif dim == 3:
+        from meshmode.mesh.generation import generate_torus
+        mesh = generate_torus(2, 1, order=target_order)
+    else:
+        raise ValueError("unsupported dimension: %d" % dim)
+
+    pre_density_discr = Discretization(
+            queue.context, mesh,
+            InterpolatoryQuadratureSimplexGroupFactory(target_order))
+
+    lpot_kwargs = DEFAULT_LPOT_KWARGS.copy()
+    lpot_kwargs.update(
+            _expansion_stick_out_factor=TCF,
+            fmm_order=FMM_ORDER,
+            qbx_order=QBX_ORDER,
+            fmm_backend="fmmlib",
+            )
+
+    from pytential.qbx import QBXLayerPotentialSource
+    lpot_source = QBXLayerPotentialSource(
+            pre_density_discr, OVSMP_FACTOR*target_order,
+            **lpot_kwargs)
+
+    lpot_source, _ = lpot_source.with_refinement()
+
+    return lpot_source
+
+
+def get_density(queue, lpot_source):
+    density_discr = lpot_source.density_discr
+    nodes = density_discr.nodes().with_queue(queue)
+    return cl.clmath.sin(10 * nodes[0])
+
+# }}}
+
+
+# {{{ test that timing data gathering can execute succesfully
+
+def test_timing_data_gathering(ctx_getter):
+    """Test that timing data gathering can execute succesfully."""
+
+    pytest.importorskip("pyfmmlib")
+
+    cl_ctx = ctx_getter()
+    queue = cl.CommandQueue(cl_ctx,
+            properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+    lpot_source = get_lpot_source(queue, 2)
+    sigma = get_density(queue, lpot_source)
+
+    sigma_sym = sym.var("sigma")
+    k_sym = LaplaceKernel(lpot_source.ambient_dim)
+    sym_op_S = sym.S(k_sym, sigma_sym, qbx_forced_limit=+1)
+
+    op_S = bind(lpot_source, sym_op_S)
+
+    timing_data = {}
+    op_S.eval(queue, dict(sigma=sigma), timing_data=timing_data)
+    assert timing_data
+    print(timing_data)
+
+# }}}
+
+
+# {{{ test cost model
+
+@pytest.mark.parametrize("dim, use_target_specific_qbx", (
+    (2, False),
+    (3, False),
+    (3, True)))
+def test_cost_model(ctx_getter, dim, use_target_specific_qbx):
+    """Test that cost model gathering can execute successfully."""
+    cl_ctx = ctx_getter()
+    queue = cl.CommandQueue(cl_ctx)
+
+    lpot_source = (
+            get_lpot_source(queue, dim)
+            .copy(
+                _use_target_specific_qbx=use_target_specific_qbx,
+                cost_model=CLQBXCostModel(queue)))
+
+    sigma = get_density(queue, lpot_source)
+
+    sigma_sym = sym.var("sigma")
+    k_sym = LaplaceKernel(lpot_source.ambient_dim)
+
+    sym_op_S = sym.S(k_sym, sigma_sym, qbx_forced_limit=+1)
+    op_S = bind(lpot_source, sym_op_S)
+    cost_S = op_S.get_modeled_cost(queue, "constant_one", sigma=sigma)
+    assert len(cost_S) == 1
+
+    sym_op_S_plus_D = (
+            sym.S(k_sym, sigma_sym, qbx_forced_limit=+1)
+            + sym.D(k_sym, sigma_sym))
+    op_S_plus_D = bind(lpot_source, sym_op_S_plus_D)
+    cost_S_plus_D = op_S_plus_D.get_modeled_cost(queue, "constant_one", sigma=sigma)
+    assert len(cost_S_plus_D) == 2
+
+# }}}
+
+
+# You can test individual routines by typing
+# $ python test_cost_model.py 'test_routine()'
 
 if __name__ == "__main__":
-    ctx_factory = cl.create_some_context
-    test_compare_cl_and_py_cost_model(ctx_factory)
+    import sys
+    if len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        from pytest import main
+        main([__file__])
+
 
 # vim: foldmethod=marker
