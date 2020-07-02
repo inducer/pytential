c738c07950cb106f58b15ec7b803ea652d0d0a3e
Matt Wala <wala1@illinois.edu>
2020-07-02 00:18:40 -0500

    Add utils.py

---
+++ pytential/utils.py
@@ -0,0 +1,46 @@
+__copyright__ = """
+Copyright (C) 2020 Matt Wala
+"""
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import numpy as np
+from meshmode.array_context import PyOpenCLArrayContext
+
+
+def flatten_if_needed(actx: PyOpenCLArrayContext, ary: np.ndarray):
+    from pytools.obj_array import obj_array_vectorize_n_args
+    from meshmode.dof_array import DOFArray, thaw, flatten
+
+    if (isinstance(ary, np.ndarray)
+            and ary.dtype.char == "O"
+            and not isinstance(ary, DOFArray)):
+        return obj_array_vectorize_n_args(flatten_if_needed, actx, ary)
+
+    if not isinstance(ary, DOFArray):
+        return ary
+
+    if ary.array_context is None:
+        ary = thaw(actx, ary)
+
+    return flatten(ary)
+
+# vim: foldmethod=marker
