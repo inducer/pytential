abdfb2b840b4a008589b06cd8f37a2a39029256b
Isuru Fernando <idf2@illinois.edu>
2021-01-04 23:59:10 -0600

    use IntG interface in test_layer_pot.py

--- test/test_layer_pot.py
+++ test/test_layer_pot.py
@@ -283,7 +283,8 @@ def test_unregularized_with_ones_kernel(ctx_factory):
 
     from sumpy.kernel import one_kernel_2d
     sigma_sym = sym.var("sigma")
-    op = sym.S(one_kernel_2d, sigma_sym, qbx_forced_limit=None)
+    op = sym.IntG(target_kernel=one_kernel_2d, source_kernels=(one_kernel_2d,),
+            densities=(sigma_sym,), qbx_forced_limit=None)
 
     sigma = discr.zeros(actx) + 1
 
