3bd6d2dac9dc2f198f1397a6ea9dc6a84740650f
Isuru Fernando <isuruf@gmail.com>
2021-01-13 19:11:25 -0600

    Use ref for source_kernels in docs
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- pytential/symbolic/compiler.py
+++ pytential/symbolic/compiler.py
@@ -154,7 +154,7 @@ class ComputePotentialInstruction(Instruction):
 
     .. attribute:: densities
 
-        a list of densities with the same number of source_kernels.
+        a list of densities with the same number of entries as :attr:`source_kernels`.
         See :class:`pytential.symbolic.primitives.IntG` docstring for details.
 
     .. attribute:: source
