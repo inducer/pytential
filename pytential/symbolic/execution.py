765d57ab8b3f2f31ca82605be33fe746b3b587e2
Alexandru Fikl <alexfikl@gmail.com>
2022-02-20 18:51:05 -0600

    remove usage of MeshElementGroup.element_nr_base

--- pytential/symbolic/execution.py
+++ pytential/symbolic/execution.py
@@ -62,22 +62,6 @@ class EvaluationMapperBoundOpCacheKey:
 
 # {{{ evaluation mapper base (shared, between actual eval and cost model)
 
-def mesh_el_view(mesh, group_nr, global_array):
-    """Return a view of *global_array* of shape
-    ``(..., mesh.groups[group_nr].nelements)``
-    where *global_array* is of shape ``(..., nelements)``,
-    where *nelements* is the global (per-mesh) element count.
-    """
-
-    group = mesh.groups[group_nr]
-
-    return global_array[
-        ..., group.element_nr_base:group.element_nr_base + group.nelements] \
-        .reshape(
-            global_array.shape[:-1]
-            + (group.nelements,))
-
-
 class EvaluationMapperBase(PymbolicEvaluationMapper):
     def __init__(self, bound_expr, actx: PyOpenCLArrayContext, context=None,
             target_geometry=None,
