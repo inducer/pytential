5ceb98f7bcda3026177ee0a2fcadf382a5a69bfc
Isuru Fernando <idf2@illinois.edu>
2022-06-27 09:39:39 -0500

    Stokes/Elasticity using biharmonic/Laplace

---
+++ pytential/symbolic/elasticity.py
@@ -0,0 +1,166 @@
+__copyright__ = "Copyright (C) 2021 Isuru Fernando"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import numpy as np
+
+from pytential import sym
+from sumpy.kernel import (AxisTargetDerivative, AxisSourceDerivative,
+    TargetPointMultiplier, LaplaceKernel)
+from pytential.symbolic.stokes import (StressletWrapperBase, StokesletWrapperBase,
+        _MU_SYM_DEFAULT)
+
+
+class StressletWrapperYoshida(StressletWrapperBase):
+    """Stresslet Wrapper using Yoshida et al's method [1] which uses Laplace
+    derivatives.
+
+    [1] Yoshida, K. I., Nishimura, N., & Kobayashi, S. (2001). Application of
+        fast multipole Galerkin boundary integral equation method to elastostatic
+        crack problems in 3D.
+        International Journal for Numerical Methods in Engineering, 50(3), 525-547.
+    """
+
+    def __init__(self, dim=None, mu_sym=_MU_SYM_DEFAULT, nu_sym=0.5):
+        self.dim = dim
+        if dim != 3:
+            raise ValueError("unsupported dimension given to "
+                "StressletWrapperYoshida")
+        self.kernel = LaplaceKernel(dim=3)
+        self.mu = mu_sym
+        self.nu = nu_sym
+
+    def apply(self, density_vec_sym, dir_vec_sym, qbx_forced_limit,
+            extra_deriv_dirs=()):
+        return self.apply_stokeslet_and_stresslet([0]*self.dim,
+            density_vec_sym, dir_vec_sym, qbx_forced_limit, 0, 1,
+            extra_deriv_dirs)
+
+    def apply_stokeslet_and_stresslet(self, stokeslet_density_vec_sym,
+            stresslet_density_vec_sym, dir_vec_sym,
+            qbx_forced_limit, stokeslet_weight, stresslet_weight,
+            extra_deriv_dirs=()):
+
+        mu = self.mu
+        nu = self.nu
+        lam = 2*nu*mu/(1-2*nu)
+        stokeslet_weight *= -1
+
+        def C(i, j, k, l):   # noqa: E741
+            res = 0
+            if i == j and k == l:
+                res += lam
+            if i == k and j == l:
+                res += mu
+            if i == l and j == k:
+                res += mu
+            return res * stresslet_weight
+
+        def add_extra_deriv_dirs(target_kernel):
+            for deriv_dir in extra_deriv_dirs:
+                target_kernel = AxisTargetDerivative(deriv_dir, target_kernel)
+            return target_kernel
+
+        def P(i, j, int_g):
+            int_g = int_g.copy(target_kernel=add_extra_deriv_dirs(
+                int_g.target_kernel))
+            res = -int_g.copy(target_kernel=TargetPointMultiplier(j,
+                    AxisTargetDerivative(i, int_g.target_kernel)))
+            if i == j:
+                res += (3 - 4*nu)*int_g
+            return res / (4*mu*(1 - nu))
+
+        def Q(i, int_g):
+            res = int_g.copy(target_kernel=add_extra_deriv_dirs(
+                AxisTargetDerivative(i, int_g.target_kernel)))
+            return res / (4*mu*(1 - nu))
+
+        sym_expr = np.zeros((3,), dtype=object)
+
+        kernel = self.kernel
+        source = [sym.NodeCoordinateComponent(d) for d in range(3)]
+        normal = dir_vec_sym
+        sigma = stresslet_density_vec_sym
+
+        source_kernels = [None]*4
+        for i in range(3):
+            source_kernels[i] = AxisSourceDerivative(i, kernel)
+        source_kernels[3] = kernel
+
+        for i in range(3):
+            for k in range(3):
+                densities = [0]*4
+                for l in range(3):   # noqa: E741
+                    for j in range(3):
+                        for m in range(3):
+                            densities[l] += C(k, l, m, j)*normal[m]*sigma[j]
+                densities[3] += stokeslet_weight * stokeslet_density_vec_sym[k]
+                int_g = sym.IntG(target_kernel=kernel,
+                    source_kernels=tuple(source_kernels),
+                    densities=tuple(densities),
+                    qbx_forced_limit=qbx_forced_limit)
+                sym_expr[i] += P(i, k, int_g)
+
+            densities = [0]*4
+            for k in range(3):
+                for m in range(3):
+                    for j in range(3):
+                        for l in range(3):   # noqa: E741
+                            densities[l] += \
+                                    C(k, l, m, j)*normal[m]*sigma[j]*source[k]
+                            if k == l:
+                                densities[3] += \
+                                        C(k, l, m, j)*normal[m]*sigma[j]
+                densities[3] += stokeslet_weight * source[k] \
+                        * stokeslet_density_vec_sym[k]
+            int_g = sym.IntG(target_kernel=kernel,
+                source_kernels=tuple(source_kernels),
+                densities=tuple(densities),
+                qbx_forced_limit=qbx_forced_limit)
+            sym_expr[i] += Q(i, int_g)
+
+        return sym_expr
+
+
+class StokesletWrapperYoshida(StokesletWrapperBase):
+    """Stokeslet Wrapper using Yoshida et al's method [1] which uses Laplace
+    derivatives.
+
+    [1] Yoshida, K. I., Nishimura, N., & Kobayashi, S. (2001). Application of
+        fast multipole Galerkin boundary integral equation method to elastostatic
+        crack problems in 3D.
+        International Journal for Numerical Methods in Engineering, 50(3), 525-547.
+    """
+
+    def __init__(self, dim=None, mu_sym=_MU_SYM_DEFAULT, nu_sym=0.5):
+        self.dim = dim
+        if dim != 3:
+            raise ValueError("unsupported dimension given to "
+                "StokesletWrapperYoshida")
+        self.kernel = LaplaceKernel(dim=3)
+        self.mu = mu_sym
+        self.nu = nu_sym
+
+    def apply(self, density_vec_sym, qbx_forced_limit, extra_deriv_dirs=()):
+        stresslet = StressletWrapperYoshida(3, self.mu, self.nu)
+        return stresslet.apply_stokeslet_and_stresslet(density_vec_sym,
+            [0]*self.dim, [0]*self.dim, qbx_forced_limit, 1, 0,
+            extra_deriv_dirs)
---
+++ pytential/symbolic/pde/system_utils.py
@@ -0,0 +1,466 @@
+__copyright__ = "Copyright (C) 2020 Isuru Fernando"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import numpy as np
+
+from sumpy.symbolic import make_sym_vector, SympyToPymbolicMapper
+import sumpy.symbolic as sym
+from sumpy.kernel import (AxisTargetDerivative, AxisSourceDerivative,
+    ExpressionKernel, KernelWrapper, TargetPointMultiplier)
+from pytools import (memoize_on_first_arg,
+                generate_nonnegative_integer_tuples_summing_to_at_most
+                as gnitstam)
+
+from pytential.symbolic.primitives import (NodeCoordinateComponent,
+    hashable_kernel_args)
+from pytential.symbolic.mappers import IdentityMapper
+from pytential.utils import chop, lu_solve_with_expand
+import pytential
+
+import logging
+logger = logging.getLogger(__name__)
+
+__all__ = (
+    "rewrite_using_base_kernel",
+    "get_deriv_relation",
+    )
+
+__doc__ = """
+.. autofunction:: rewrite_using_base_kernel
+.. autofunction:: get_deriv_relation
+"""
+
+
+# {{{ rewrite_using_base_kernel
+
+_NO_ARG_SENTINEL = object()
+
+
+def rewrite_using_base_kernel(exprs, base_kernel=_NO_ARG_SENTINEL):
+    """Rewrites an expression with :class:`~pytential.symbolic.primitives.IntG`
+    objects using *base_kernel*.
+
+    For example, if *base_kernel* is the Biharmonic kernel, and a Laplace kernel
+    is encountered, this will (forcibly) rewrite the Laplace kernel in terms of
+    derivatives of the Biharmonic kernel.
+
+    The routine will fail if this process cannot be completed.
+    """
+    if base_kernel is None:
+        return list(exprs)
+    mapper = RewriteUsingBaseKernelMapper(base_kernel)
+    return [mapper(expr) for expr in exprs]
+
+
+class RewriteUsingBaseKernelMapper(IdentityMapper):
+    """Rewrites IntGs using the base kernel. First this method replaces
+    IntGs with :class:`sumpy.kernel.AxisTargetDerivative` to IntGs
+    :class:`sumpy.kernel.AxisSourceDerivative` and then replaces
+    IntGs with :class:`sumpy.kernel.TargetPointMultiplier` to IntGs
+    without them using :class:`sumpy.kernel.ExpressionKernel`
+    and then finally converts them to the base kernel by finding
+    a relationship between the derivatives.
+    """
+    def __init__(self, base_kernel):
+        self.base_kernel = base_kernel
+
+    def map_int_g(self, expr):
+        # First convert IntGs with target derivatives to source derivatives
+        expr = convert_target_deriv_to_source(expr)
+        # Convert IntGs with TargetMultiplier to a sum of IntGs without
+        # TargetMultipliers
+        new_int_gs = convert_target_multiplier_to_source(expr)
+        # Convert IntGs with different kernels to expressions containing
+        # IntGs with base_kernel or its derivatives
+        return sum(convert_int_g_to_base(new_int_g,
+            self.base_kernel) for new_int_g in new_int_gs)
+
+
+def convert_target_deriv_to_source(int_g):
+    """Converts AxisTargetDerivatives to AxisSourceDerivative instances
+    from an IntG. If there are outer TargetPointMultiplier transformations
+    they are preserved.
+    """
+    knl = int_g.target_kernel
+    source_kernels = list(int_g.source_kernels)
+    coeff = 1
+    multipliers = []
+    while isinstance(knl, TargetPointMultiplier):
+        multipliers.append(knl.axis)
+        knl = knl.inner_kernel
+
+    while isinstance(knl, AxisTargetDerivative):
+        coeff *= -1
+        source_kernels = [AxisSourceDerivative(knl.axis, source_knl) for
+                source_knl in source_kernels]
+        knl = knl.inner_kernel
+
+    # TargetPointMultiplier has to be the outermost kernel
+    # If it is the inner kernel, return early
+    if isinstance(knl, TargetPointMultiplier):
+        return int_g
+
+    for axis in reversed(multipliers):
+        knl = TargetPointMultiplier(axis, knl)
+
+    new_densities = tuple(density*coeff for density in int_g.densities)
+    return int_g.copy(target_kernel=knl,
+                      densities=new_densities,
+                      source_kernels=tuple(source_kernels))
+
+
+def _get_kernel_expression(expr, kernel_arguments):
+    from pymbolic.mapper.substitutor import substitute
+    from sumpy.symbolic import PymbolicToSympyMapperWithSymbols
+
+    pymbolic_expr = substitute(expr, kernel_arguments)
+
+    res = PymbolicToSympyMapperWithSymbols()(pymbolic_expr)
+    return res
+
+
+def _monom_to_expr(monom, variables):
+    prod = 1
+    for i, nrepeats in enumerate(monom):
+        for _ in range(nrepeats):
+            prod *= variables[i]
+    return prod
+
+
+def convert_target_multiplier_to_source(int_g):
+    """Convert an IntG with TargetMultiplier to a sum of IntGs without
+    TargetMultiplier and only source dependent transformations
+    """
+    import sympy
+    import sumpy.symbolic as sym
+    from sumpy.symbolic import SympyToPymbolicMapper
+    conv = SympyToPymbolicMapper()
+
+    knl = int_g.target_kernel
+    # we use a symbol for d = (x - y)
+    ds = sympy.symbols(f"d0:{knl.dim}")
+    sources = sympy.symbols(f"y0:{knl.dim}")
+    # instead of just x, we use x = (d + y)
+    targets = [d + source for d, source in zip(ds, sources)]
+    orig_expr = sympy.Function("f")(*ds)  # pylint: disable=not-callable
+    expr = orig_expr
+    found = False
+    while isinstance(knl, KernelWrapper):
+        if isinstance(knl, TargetPointMultiplier):
+            expr = targets[knl.axis] * expr
+            found = True
+        elif isinstance(knl, AxisTargetDerivative):
+            # sympy can't differentiate w.r.t target because
+            # it's not a symbol, but d/d(x) = d/d(d)
+            expr = expr.diff(ds[knl.axis])
+        else:
+            return [int_g]
+        knl = knl.inner_kernel
+
+    if not found:
+        return [int_g]
+
+    sources_pymbolic = [NodeCoordinateComponent(i) for i in range(knl.dim)]
+    expr = expr.expand()
+    # Now the expr is an Add and looks like
+    # u''(d, s)*d[0] + u(d, s)
+    assert isinstance(expr, sympy.Add)
+    result = []
+
+    for arg in expr.args:
+        deriv_terms = arg.atoms(sympy.Derivative)
+        if len(deriv_terms) == 1:
+            deriv_term = deriv_terms.pop()
+            rest_terms = sympy.Poly(arg.xreplace({deriv_term: 1}), *ds, *sources)
+            derivatives = deriv_term.args[1:]
+        elif len(deriv_terms) == 0:
+            rest_terms = sympy.Poly(arg.xreplace({orig_expr: 1}), *ds, *sources)
+            derivatives = [(d, 0) for d in ds]
+        else:
+            raise AssertionError("impossible condition")
+        assert len(rest_terms.terms()) == 1
+        monom, coeff = rest_terms.terms()[0]
+        expr_multiplier = _monom_to_expr(monom[:len(ds)], ds)
+        density_multiplier = _monom_to_expr(monom[len(ds):], sources_pymbolic) \
+                * conv(coeff)
+
+        new_int_gs = _multiply_int_g(int_g, sym.sympify(expr_multiplier),
+                density_multiplier)
+        for new_int_g in new_int_gs:
+            knl = new_int_g.target_kernel
+            for axis_var, nrepeats in derivatives:
+                axis = ds.index(axis_var)
+                for _ in range(nrepeats):
+                    knl = AxisTargetDerivative(axis, knl)
+            result.append(new_int_g.copy(target_kernel=knl))
+    return result
+
+
+def _multiply_int_g(int_g, expr_multiplier, density_multiplier):
+    """Multiply the exprssion in IntG with the *expr_multiplier*
+    which is a symbolic expression and multiply the densities
+    with *density_multiplier* which is a pymbolic expression.
+    """
+    from sumpy.symbolic import SympyToPymbolicMapper
+    result = []
+
+    base_kernel = int_g.target_kernel.get_base_kernel()
+    sym_d = make_sym_vector("d", base_kernel.dim)
+    base_kernel_expr = _get_kernel_expression(base_kernel.expression,
+            int_g.kernel_arguments)
+    conv = SympyToPymbolicMapper()
+
+    for knl, density in zip(int_g.source_kernels, int_g.densities):
+        if expr_multiplier == 1:
+            new_knl = knl.get_base_kernel()
+        else:
+            new_expr = conv(knl.postprocess_at_source(base_kernel_expr, sym_d)
+                    * expr_multiplier)
+            new_knl = ExpressionKernel(knl.dim, new_expr,
+                knl.get_base_kernel().global_scaling_const,
+                knl.is_complex_valued)
+        result.append(int_g.copy(target_kernel=new_knl,
+            densities=(density*density_multiplier,),
+            source_kernels=(new_knl,)))
+    return result
+
+
+def convert_int_g_to_base(int_g, base_kernel):
+    result = 0
+    for knl, density in zip(int_g.source_kernels, int_g.densities):
+        result += _convert_int_g_to_base(
+                int_g.copy(source_kernels=(knl,), densities=(density,)),
+                base_kernel)
+    return result
+
+
+def _convert_int_g_to_base(int_g, base_kernel):
+    target_kernel = int_g.target_kernel.replace_base_kernel(base_kernel)
+    dim = target_kernel.dim
+
+    result = 0
+    for density, source_kernel in zip(int_g.densities, int_g.source_kernels):
+        deriv_relation = get_deriv_relation_kernel(source_kernel.get_base_kernel(),
+            base_kernel, hashable_kernel_arguments=(
+                hashable_kernel_args(int_g.kernel_arguments)))
+
+        const = deriv_relation[0]
+        # NOTE: we set a dofdesc here to force the evaluation of this integral
+        # on the source instead of the target when using automatic tagging
+        # see :meth:`pytential.symbolic.mappers.LocationTagger._default_dofdesc`
+        dd = pytential.sym.DOFDescriptor(None,
+                discr_stage=pytential.sym.QBX_SOURCE_STAGE1)
+        const *= pytential.sym.integral(dim, dim-1, density, dofdesc=dd)
+
+        if const != 0 and target_kernel != target_kernel.get_base_kernel():
+            # There might be some TargetPointMultipliers hanging around.
+            # FIXME: handle them instead of bailing out
+            return [int_g]
+
+        if source_kernel != source_kernel.get_base_kernel():
+            # We assume that any source transformation is a derivative
+            # and the constant when applied becomes zero.
+            const = 0
+
+        result += const
+
+        new_kernel_args = filter_kernel_arguments([base_kernel],
+                int_g.kernel_arguments)
+
+        for mi, c in deriv_relation[1]:
+            knl = source_kernel.replace_base_kernel(base_kernel)
+            for d, val in enumerate(mi):
+                for _ in range(val):
+                    knl = AxisSourceDerivative(d, knl)
+                    c *= -1
+            result += int_g.copy(source_kernels=(knl,), target_kernel=target_kernel,
+                    densities=(density * c,), kernel_arguments=new_kernel_args)
+    return result
+
+
+def get_deriv_relation(kernels, base_kernel, tol=1e-10, order=None,
+        kernel_arguments=None):
+    res = []
+    for knl in kernels:
+        res.append(get_deriv_relation_kernel(knl, base_kernel, tol, order,
+            hashable_kernel_arguments=hashable_kernel_args(kernel_arguments)))
+    return res
+
+
+@memoize_on_first_arg
+def get_deriv_relation_kernel(kernel, base_kernel, tol=1e-10, order=None,
+        hashable_kernel_arguments=None):
+    kernel_arguments = dict(hashable_kernel_arguments)
+    (L, U, perm), rand, mis = _get_base_kernel_matrix(base_kernel, order=order)
+    dim = base_kernel.dim
+    sym_vec = make_sym_vector("d", dim)
+    sympy_conv = SympyToPymbolicMapper()
+
+    expr = _get_kernel_expression(kernel.expression, kernel_arguments)
+    vec = []
+    for i in range(len(mis)):
+        vec.append(evalf(expr.xreplace(dict((k, v) for
+            k, v in zip(sym_vec, rand[:, i])))))
+    vec = sym.Matrix(vec)
+    result = []
+    const = 0
+    logger.debug("%s = ", kernel)
+
+    sol = lu_solve_with_expand(L, U, perm, vec)
+    for i, coeff in enumerate(sol):
+        coeff = chop(coeff, tol)
+        if coeff == 0:
+            continue
+        if mis[i] != (-1, -1, -1):
+            coeff *= _get_kernel_expression(kernel.global_scaling_const,
+                    kernel_arguments)
+            coeff /= _get_kernel_expression(base_kernel.global_scaling_const,
+                    kernel_arguments)
+            result.append((mis[i], sympy_conv(coeff)))
+            logger.debug("  + %s.diff(%s)*%s", base_kernel, mis[i], coeff)
+        else:
+            const = sympy_conv(coeff * _get_kernel_expression(
+                kernel.global_scaling_const, kernel_arguments))
+    logger.debug("  + %s", const)
+    return (const, result)
+
+
+@memoize_on_first_arg
+def _get_base_kernel_matrix(base_kernel, order=None, retries=3,
+        kernel_arguments=None):
+    dim = base_kernel.dim
+
+    pde = base_kernel.get_pde_as_diff_op()
+    if order is None:
+        order = pde.order
+
+    if order > pde.order:
+        raise NotImplementedError(f"order ({order}) cannot be greater than the order"
+                         f"of the PDE ({pde.order}) yet.")
+
+    mis = sorted(gnitstam(order, dim), key=sum)
+    # (-1, -1, -1) represent a constant
+    mis.append((-1, -1, -1))
+
+    if order == pde.order:
+        pde_mis = [ident.mi for eq in pde.eqs for ident in eq.keys()]
+        pde_mis = [mi for mi in pde_mis if sum(mi) == order]
+        logger.debug(f"Removing {pde_mis[-1]} to avoid linear dependent mis")
+        mis.remove(pde_mis[-1])
+
+    rand = np.random.randint(1, 10**15, (dim, len(mis)))
+    rand = rand.astype(object)
+    for i in range(rand.shape[0]):
+        for j in range(rand.shape[1]):
+            rand[i, j] = sym.sympify(rand[i, j])/10**15
+    sym_vec = make_sym_vector("d", dim)
+
+    base_expr = _get_kernel_expression(base_kernel.expression, kernel_arguments)
+
+    mat = []
+    for rand_vec_idx in range(rand.shape[1]):
+        row = []
+        for mi in mis[:-1]:
+            expr = base_expr
+            for var_idx, nderivs in enumerate(mi):
+                if nderivs == 0:
+                    continue
+                expr = expr.diff(sym_vec[var_idx], nderivs)
+            replace_dict = dict(
+                (k, v) for k, v in zip(sym_vec, rand[:, rand_vec_idx])
+            )
+            eval_expr = evalf(expr.xreplace(replace_dict))
+            row.append(eval_expr)
+        row.append(1)
+        mat.append(row)
+
+    mat = sym.Matrix(mat)
+    failed = False
+    try:
+        L, U, perm = mat.LUdecomposition()
+    except RuntimeError:
+        # symengine throws an error when rank deficient
+        # and sympy returns U with last row zero
+        failed = True
+
+    if not sym.USE_SYMENGINE and all(expr == 0 for expr in U[-1, :]):
+        failed = True
+
+    if failed:
+        if retries == 0:
+            raise RuntimeError("Failed to find a base kernel")
+        return _get_base_kernel_matrix(
+            base_kernel=base_kernel,
+            order=order,
+            retries=retries-1,
+        )
+
+    return (L, U, perm), rand, mis
+
+
+def evalf(expr, prec=100):
+    """evaluate an expression numerically using ``prec``
+    number of bits.
+    """
+    from sumpy.symbolic import USE_SYMENGINE
+    if USE_SYMENGINE:
+        return expr.n(prec=prec)
+    else:
+        import sympy
+        dps = int(sympy.log(2**prec, 10))
+        return expr.n(n=dps)
+
+
+def filter_kernel_arguments(knls, kernel_arguments):
+    """From a dictionary of kernel arguments, filter out arguments
+    that are not needed for the kernels given as a list and return a new
+    dictionary.
+    """
+    kernel_arg_names = set()
+
+    for kernel in knls:
+        for karg in (kernel.get_args() + kernel.get_source_args()):
+            kernel_arg_names.add(karg.loopy_arg.name)
+
+    return {k: v for (k, v) in kernel_arguments.items() if k in kernel_arg_names}
+
+# }}}
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    from sumpy.kernel import (StokesletKernel, BiharmonicKernel,  # noqa:F401
+        StressletKernel, ElasticityKernel, LaplaceKernel)
+    base_kernel = BiharmonicKernel(3)
+    #base_kernel = LaplaceKernel(3)
+    kernels = [StokesletKernel(3, 0, 2), StokesletKernel(3, 0, 0)]
+    kernels += [StressletKernel(3, 0, 0, 0), StressletKernel(3, 0, 0, 1),
+            StressletKernel(3, 0, 0, 2), StressletKernel(3, 0, 1, 2)]
+
+    sym_d = make_sym_vector("d", base_kernel.dim)
+    sym_r = sym.sqrt(sum(a**2 for a in sym_d))
+    conv = SympyToPymbolicMapper()
+    expression_knl = ExpressionKernel(3, conv(sym_d[0]*sym_d[1]/sym_r**3), 1, False)
+    expression_knl2 = ExpressionKernel(3, conv(1/sym_r + sym_d[0]*sym_d[0]/sym_r**3),
+        1, False)
+    kernels = [expression_knl, expression_knl2]
+    get_deriv_relation(kernels, base_kernel, tol=1e-10, order=4)
