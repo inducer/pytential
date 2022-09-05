83d304783cb9432906f68c4f2ccbee0528b88680
Isuru Fernando <idf2@illinois.edu>
2022-09-05 16:44:16 -0500

    simplify loops

--- pytential/symbolic/stokes.py
+++ pytential/symbolic/stokes.py
@@ -303,17 +303,14 @@ class _StokesletWrapperNaiveOrBiharmonic(StokesletWrapperBase):
         # ElasticityKernel needs to know if nu=0.5 or not at creation time
         poisson_ratio = "nu" if nu_sym != 0.5 else 0.5
 
-        for i in range(dim):
-            for j in range(i, dim):
-                self.kernel_dict[(i, j)] = ElasticityKernel(dim=dim, icomp=i,
-                    jcomp=j, poisson_ratio=poisson_ratio)
-
         # The dictionary allows us to exploit symmetry -- that
         # :math:`T_{01}` is identical to :math:`T_{10}` -- and avoid creating
         # multiple expansions for the same kernel in a different ordering.
         for i in range(dim):
-            for j in range(i):
-                self.kernel_dict[(i, j)] = self.kernel_dict[(j, i)]
+            for j in range(i, dim):
+                self.kernel_dict[(i, j)] = ElasticityKernel(dim=dim, icomp=i,
+                    jcomp=j, poisson_ratio=poisson_ratio)
+                self.kernel_dict[(j, i)] = self.kernel_dict[(i, j)]
 
     def get_int_g(self, idx, density_sym, dir_vec_sym, qbx_forced_limit,
             deriv_dirs):
