86a516b352626396cf756b7c93855061fc5ec849
Isuru Fernando <isuruf@gmail.com>
2022-12-19 21:08:10 -0600

    fix bad merge

--- pytential/symbolic/mappers.py
+++ pytential/symbolic/mappers.py
@@ -290,6 +290,10 @@ class LocationTagger(CSECachingMapperMixin, IdentityMapper):
                 dofdesc = dofdesc.copy(geometry=self.default_target)
             else:
                 dofdesc = dofdesc.copy(geometry=self.default_source)
+        elif dofdesc.geometry is prim.DEFAULT_SOURCE:
+            dofdesc = dofdesc.copy(geometry=self.default_source)
+        elif dofdesc.geometry is prim.DEFAULT_TARGET:
+            dofdesc = dofdesc.copy(geometry=self.default_target)
 
         return dofdesc
 
