329e6eaf07e921af2a062f7dc70fba81d1c48603
Alexandru Fikl <alexfikl@gmail.com>
2023-06-25 21:15:00 +0300

    target: make TargetBase an ABC and add docs

--- pytential/target.py
+++ pytential/target.py
@@ -33,42 +33,83 @@ used as evaluation targets.
 
 """
 
+from abc import ABC, abstractmethod
+from typing import Optional
 
-class TargetBase:
-    """
-    .. attribute:: ambient_dim
-    .. method:: nodes
+from arraycontext import Array
+from pytools import T
 
-        Shape: ``[ambient_dim, ndofs]``
-    .. attribute:: ndofs
+from pytential.collection import GeometryCollection
+
+
+class TargetBase(ABC):
     """
+    .. autoproperty:: ambient_dim
+    .. autoproperty:: ndofs
+    .. automethod:: nodes
+    """
+
+    @property
+    @abstractmethod
+    def ambient_dim(self) -> int:
+        """Ambient dimension of the points in the target geometry."""
+
+    @property
+    @abstractmethod
+    def ndofs(self) -> int:
+        """Number of points (DOFs) in the target geometry."""
+
+    @abstractmethod
+    def nodes(self) -> Array:
+        """
+        :returns: an array of points of shape ``[ambient_dim, ndofs]`` that
+            form the target geometry.
+        """
 
 
 class PointsTarget(TargetBase):
-    """The point of this class is to act as a container for some target points
+    """A generic container for a set of target points.
+
+    The point of this class is to act as a container for some target points
     while presenting enough of the :class:`meshmode.discretization.Discretization`
     interface to not necessitate a lot of special cases in that code path.
+
+    .. attribute:: normals
+        :type: Optional[Array]
+
+        An array of the same shape as :meth:`TargetBase.nodes` that gives the
+        normals at each of the target points.
+
+    .. automethod:: preprocess_optemplate
     """
 
-    def __init__(self, nodes, normals=None):
+    def __init__(self, nodes: Array, normals: Optional[Array] = None) -> None:
         self._nodes = nodes
+        self.normals = normals
 
     @property
-    def ambient_dim(self):
+    def ambient_dim(self) -> int:
         return self._nodes.shape[0]
 
-    def preprocess_optemplate(self, name, discretizations, expr):
-        return expr
+    @property
+    def ndofs(self) -> int:
+        # NOTE: arraycontext.Array is not iterable theoretically
+        for coord_ary in self._nodes:   # type: ignore[attr-defined]
+            return coord_ary.shape[0]
 
-    def nodes(self):
-        """Shape: ``[ambient_dim, ndofs]``
-        """
+        raise AttributeError
 
+    def nodes(self) -> Array:
         return self._nodes
 
-    @property
-    def ndofs(self):
-        for coord_ary in self._nodes:
-            return coord_ary.shape[0]
+    def preprocess_optemplate(self,
+                name: str,
+                discretizations: GeometryCollection,
+                # FIXME: replace this with a pymbolic TypeVar bound to an actual
+                # expression when that gets in
+                expr: T) -> T:
+        """See :class:`~pytential.source.PotentialSource`."""
+        return expr
+
 
 # vim: foldmethod=marker
