9bb27575ccc1ea8000a27c1c9768e02d07314775
Alexandru Fikl <alexfikl@gmail.com>
2024-07-02 20:23:01 +0300

    pyproject: move tool configuration

--- setup.py
+++ setup.py
@@ -1,68 +1,10 @@
 #!/usr/bin/env python
 
-import os
 import sys
 
 from Cython.Build import cythonize
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.extension import Extension
-
-
-# {{{ capture git revision at install time
-
-def find_git_revision(tree_root):
-    # authoritative version in pytools/__init__.py
-    # Keep this routine self-contained so that it can be copy-pasted into
-    # setup.py.
-
-    from os.path import abspath, exists, join
-
-    tree_root = abspath(tree_root)
-
-    if not exists(join(tree_root, ".git")):
-        return None
-
-    from subprocess import PIPE, STDOUT, Popen
-
-    p = Popen(
-        ["git", "rev-parse", "HEAD"],
-        shell=False,
-        stdin=PIPE,
-        stdout=PIPE,
-        stderr=STDOUT,
-        close_fds=True,
-        cwd=tree_root,
-    )
-    (git_rev, _) = p.communicate()
-
-    git_rev = git_rev.decode()
-    git_rev = git_rev.rstrip()
-
-    retcode = p.returncode
-    assert retcode is not None
-    if retcode != 0:
-        from warnings import warn
-
-        warn("unable to find git revision")
-        return None
-
-    return git_rev
-
-
-def write_git_revision(package_name):
-    from os.path import dirname, join
-
-    dn = dirname(__file__)
-    git_rev = find_git_revision(dn)
-
-    with open(join(dn, package_name, "_git_rev.py"), "w") as outf:
-        outf.write("GIT_REVISION = %s\n" % repr(git_rev))
-
-
-write_git_revision("pytential")
-
-# }}}
-
 
 if sys.platform.startswith("linux"):
     openmp_flag = ["-fopenmp"]
@@ -88,56 +30,4 @@ ext_modules = [
     ),
 ]
 
-
-version_dict = {}
-init_filename = "pytential/version.py"
-os.environ["AKPYTHON_EXEC_FROM_WITHIN_WITHIN_SETUP_PY"] = "1"
-exec(compile(open(init_filename).read(), init_filename, "exec"), version_dict)
-
-setup(
-    name="pytential",
-    version=version_dict["VERSION_TEXT"],
-    description="Evaluate layer and volume potentials accurately. "
-    "Solve integral equations.",
-    long_description=open("README.rst").read(),
-    author="Andreas Kloeckner",
-    author_email="inform@tiker.net",
-    license="MIT",
-    url="https://github.com/inducer/pytential",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Other Audience",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Visualization",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities",
-    ],
-    packages=find_packages(),
-    ext_modules=cythonize(ext_modules),
-    python_requires=">=3.8",
-    install_requires=[
-        "pytools>=2018.2",
-        "modepy>=2021.1",
-        "pyopencl>=2021.2.6",
-        "boxtree>=2019.1",
-        "pymbolic>=2022.1",
-        "loopy>=2020.2",
-        "arraycontext>=2021.1",
-        "meshmode>=2021.2",
-        "sumpy>=2020.2beta1",
-
-        "scipy>=1.2.0",
-        "immutables",
-    ],
-    extras_require={
-        "fmmlib": ["pyfmmlib>=2019.1.1"],
-    },
-)
+setup(ext_modules=cythonize(ext_modules))
