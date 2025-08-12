89521ce04f46f47d5a1df5a41c7192ee24604c9a
Alexandru Fikl <alexfikl@gmail.com>
2025-08-12 20:13:56 +0300

    feat(typing): add and simplify types in dof_connection

--- pytential/symbolic/dof_connection.py
+++ pytential/symbolic/dof_connection.py
@@ -26,19 +26,36 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+from typing import TYPE_CHECKING
+
 import numpy as np  # noqa: F401
+from typing_extensions import override
 
 import loopy as lp
+from meshmode.discretization import Discretization
+from meshmode.discretization.connection import (
+    DiscretizationConnection,
+    IdentityDiscretizationConnection,
+)
 from meshmode.dof_array import DOFArray
 from pytools import memoize_in
 
+from pytential.symbolic import dof_desc
+
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from arraycontext import Array, ArrayOrContainerOrScalarT
+
+    from pytential.collection import GeometryCollection
+    from pytential.symbolic.dof_desc import DOFDescriptor, DOFDescriptorLike
 
 __doc__ = """
 
 Connections
 -----------
 
-.. autoclass:: GranularityConnection
 .. autoclass:: CenterGranularityConnection
 .. autoclass:: DOFConnection
 .. autofunction:: connection_from_dds
@@ -48,31 +65,9 @@ Connections
 
 # {{{ granularity connections
 
-class GranularityConnection:
-    """Abstract interface for transporting a DOF between different levels
-    of granularity.
-
-    .. attribute:: discr
-    .. automethod:: __call__
-    """
-
-    def __init__(self, discr):
-        self.discr = discr
-
-    @property
-    def from_discr(self):
-        return self.discr
-
-    @property
-    def to_discr(self):
-        return self.discr
-
-    def __call__(self, ary):
-        raise NotImplementedError()
-
-
-class CenterGranularityConnection(GranularityConnection):
-    """A :class:`GranularityConnection` used to transport from node data
+class CenterGranularityConnection(DiscretizationConnection):
+    """A :class:`~meshmode.discretization.connection.DiscretizationConnection`
+    used to transport from node data
     (:class:`~pytential.symbolic.primitives.GRANULARITY_NODE`) to expansion
     centers (:class:`~pytential.symbolic.primitives.GRANULARITY_CENTER`).
 
@@ -80,20 +75,22 @@ class CenterGranularityConnection(GranularityConnection):
     .. automethod:: __call__
     """
 
-    def __init__(self, discr):
-        super().__init__(discr)
+    def __init__(self, discr: Discretization) -> None:
+        super().__init__(discr, discr, is_surjective=False)
 
-    def _interleave_dof_arrays(self, ary1, ary2):
+    def _interleave_dof_arrays(self, ary1: DOFArray, ary2: DOFArray) -> DOFArray:
         if not isinstance(ary1, DOFArray) or not isinstance(ary2, DOFArray):
             raise TypeError("non-array passed to connection")
 
         if ary1.array_context is not ary2.array_context:
             raise ValueError("array context of the two arguments must match")
 
+        if ary1.array_context is None:
+            raise ValueError("cannot transport frozen arrays")
+
         actx = ary1.array_context
 
-        @memoize_in(actx,
-                 (CenterGranularityConnection, "interleave"))
+        @memoize_in(actx, (CenterGranularityConnection, "interleave"))
         def prg():
             from arraycontext import make_loopy_program
             t_unit = make_loopy_program(
@@ -117,8 +114,9 @@ class CenterGranularityConnection(GranularityConnection):
                 "iel": ConcurrentElementInameTag(),
                 "idof": ConcurrentDOFInameTag()})
 
-        results = []
-        for grp, subary1, subary2 in zip(self.discr.groups, ary1, ary2, strict=True):
+        discr = self.from_discr
+        results: list[Array] = []
+        for grp, subary1, subary2 in zip(discr.groups, ary1, ary2, strict=True):
             if subary1.dtype != subary2.dtype:
                 raise ValueError("dtype mismatch in inputs: "
                     f"'{subary1.dtype.name}' and '{subary2.dtype.name}'")
@@ -135,7 +133,8 @@ class CenterGranularityConnection(GranularityConnection):
 
         return DOFArray(actx, tuple(results))
 
-    def __call__(self, arys):
+    @override
+    def __call__(self, arys: ArrayOrContainerOrScalarT) -> ArrayOrContainerOrScalarT:
         r"""
         :param arys: a pair of :class:`~arraycontext.ArrayContainer`-like
             classes. Can also be a single element, in which case it is
@@ -165,17 +164,15 @@ class CenterGranularityConnection(GranularityConnection):
 
 # {{{ dof connection
 
-class DOFConnection:
+class DOFConnection(DiscretizationConnection):
     """An interpolation operation for converting a DOF vector between
     different DOF types, as described by
     :class:`~pytential.symbolic.dof_desc.DOFDescriptor`.
 
     .. attribute:: connections
 
-        A list of
-        :class:`~meshmode.discretization.connection.DiscretizationConnection`s
-        and :class:`GranularityConnection`s used to transport from the given
-        source to the target.
+        A list of :class:`~meshmode.discretization.connection.DiscretizationConnection`s
+        used to transport from the given source to the target.
 
     .. attribute:: from_dd
 
@@ -193,29 +190,42 @@ class DOFConnection:
     .. automethod:: __call__
     """
 
-    def __init__(self, connections, from_dd=None, to_dd=None):
-        self.from_dd = from_dd
-        self.to_dd = to_dd
-        self.connections = connections
+    from_dd: DOFDescriptor
+    to_dd: DOFDescriptor
+    connections: tuple[DiscretizationConnection, ...]
+
+    def __init__(self,
+                 connections: Sequence[DiscretizationConnection],
+                 from_dd: DOFDescriptorLike | None = None,
+                 to_dd: DOFDescriptorLike | None = None) -> None:
+        self.from_dd = dof_desc.as_dofdesc(from_dd)
+        self.to_dd = dof_desc.as_dofdesc(to_dd)
+        self.connections = tuple(connections)
+
+        if not self.connections:
+            raise ValueError(
+                "no connections given (use 'IdentityDiscretizationConnection')")
 
         from meshmode.discretization.connection import DiscretizationConnection
         for conn in self.connections:
-            if not isinstance(conn,
-                    DiscretizationConnection | GranularityConnection):
-                raise ValueError("unsupported connection type: {type(conn)}")
+            if not isinstance(conn, DiscretizationConnection):
+                raise ValueError(f"unsupported connection type: {type(conn)}")
 
-        if self.connections:
-            self.from_discr = self.connections[0].from_discr
-            self.to_discr = self.connections[-1].to_discr
+        from_discr = self.connections[0].from_discr
+        to_discr = self.connections[-1].to_discr
+        super().__init__(from_discr, to_discr, is_surjective=False)
 
-    def __call__(self, ary):
+    @override
+    def __call__(self, ary: ArrayOrContainerOrScalarT) -> ArrayOrContainerOrScalarT:
         for conn in self.connections:
             ary = conn(ary)
 
         return ary
 
 
-def connection_from_dds(places, from_dd, to_dd):
+def connection_from_dds(places: GeometryCollection,
+                        from_dd: DOFDescriptorLike,
+                        to_dd: DOFDescriptorLike) -> DiscretizationConnection:
     """
     :arg places: a :class:`~pytential.collection.GeometryCollection`
         or an argument taken by its constructor.
@@ -229,9 +239,8 @@ def connection_from_dds(places, from_dd, to_dd):
         kinds of DOF vectors.
     """
 
-    from pytential import sym
-    from_dd = sym.as_dofdesc(from_dd)
-    to_dd = sym.as_dofdesc(to_dd)
+    from_dd = dof_desc.as_dofdesc(from_dd)
+    to_dd = dof_desc.as_dofdesc(to_dd)
 
     from pytential import GeometryCollection
     if not isinstance(places, GeometryCollection):
@@ -241,31 +250,33 @@ def connection_from_dds(places, from_dd, to_dd):
     from_discr = places.get_discretization(from_dd.geometry, from_dd.discr_stage)
     to_discr = places.get_discretization(to_dd.geometry, to_dd.discr_stage)
 
+    assert isinstance(from_discr, Discretization)
+    assert isinstance(to_discr, Discretization)
+
     if from_dd.geometry != to_dd.geometry:
         raise ValueError("cannot interpolate between different geometries")
 
-    if from_dd.granularity is not sym.GRANULARITY_NODE:
+    if from_dd.granularity is not dof_desc.GRANULARITY_NODE:
         raise ValueError("can only interpolate from `GRANULARITY_NODE`")
 
-    connections = []
+    connections: list[DiscretizationConnection] = []
     if from_dd.discr_stage is not to_dd.discr_stage:
         from pytential.qbx import QBXLayerPotentialSource
         if not isinstance(lpot, QBXLayerPotentialSource):
             raise ValueError("can only interpolate on a "
                     "`QBXLayerPotentialSource`")
 
-        if to_dd.discr_stage is not sym.QBX_SOURCE_QUAD_STAGE2:
+        if to_dd.discr_stage is not dof_desc.QBX_SOURCE_QUAD_STAGE2:
             # TODO: can probably extend this to project from a QUAD_STAGE2
             # using L2ProjectionInverseDiscretizationConnection
-            raise ValueError("can only interpolate to "
-                "`QBX_SOURCE_QUAD_STAGE2`")
+            raise ValueError("can only interpolate to `QBX_SOURCE_QUAD_STAGE2`")
 
         # FIXME: would be nice if these were ordered by themselves
         stage_name_to_index_map = {
                 None: 0,
-                sym.QBX_SOURCE_STAGE1: 1,
-                sym.QBX_SOURCE_STAGE2: 2,
-                sym.QBX_SOURCE_QUAD_STAGE2: 3
+                dof_desc.QBX_SOURCE_STAGE1: 1,
+                dof_desc.QBX_SOURCE_STAGE2: 2,
+                dof_desc.QBX_SOURCE_QUAD_STAGE2: 3
                 }
         stage_index_to_name_map = {
                 i: name for name, i in stage_name_to_index_map.items()}
@@ -274,24 +285,28 @@ def connection_from_dds(places, from_dd, to_dd):
         to_stage = stage_name_to_index_map[to_dd.discr_stage]
 
         for istage in range(from_stage, to_stage):
-            conn = places._get_conn_from_cache(from_dd.geometry,
+            conn = places._get_conn_from_cache(
+                    from_dd.geometry,
                     stage_index_to_name_map[istage],
                     stage_index_to_name_map[istage + 1])
             connections.append(conn)
 
     if from_dd.granularity is not to_dd.granularity:
-        if to_dd.granularity is sym.GRANULARITY_NODE:
+        if to_dd.granularity is dof_desc.GRANULARITY_NODE:
             pass
-        elif to_dd.granularity is sym.GRANULARITY_CENTER:
+        elif to_dd.granularity is dof_desc.GRANULARITY_CENTER:
             connections.append(CenterGranularityConnection(to_discr))
-        elif to_dd.granularity is sym.GRANULARITY_ELEMENT:
+        elif to_dd.granularity is dof_desc.GRANULARITY_ELEMENT:
             raise ValueError("Creating a connection to element granularity "
                     "is not allowed. Use Elementwise{Max,Min,Sum}.")
         else:
             raise ValueError(f"invalid to_dd granularity: {to_dd.granularity}")
 
     if from_dd.granularity is not to_dd.granularity:
-        conn = DOFConnection(connections, from_dd=from_dd, to_dd=to_dd)
+        if connections:
+            conn = DOFConnection(connections, from_dd=from_dd, to_dd=to_dd)
+        else:
+            conn = IdentityDiscretizationConnection(from_discr)
     else:
         from meshmode.discretization.connection import ChainedDiscretizationConnection
         conn = ChainedDiscretizationConnection(connections,
